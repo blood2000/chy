0e0982973483d4ed4596f268df9c5cbd49255eef
ganjq <77923857@qq.com>
2021-02-18 09:38:12 +0800

    初始化工程

---
+++ src/components/Ddc/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="question" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ddcDoc',
+  data() {
+    return {
+      url: 'https://ddcwl.com/'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
