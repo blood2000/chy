ae7bd748ccae74894646011600505aa347a94d45
tinkaikin <tinkaikin@163.com>
2021-03-09 11:09:18 +0800

    样式1

--- src/views/system/testlog/index.vue
+++ src/views/system/testlog/index.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>123</div>
+  <div >
+    <el-row>
+      <el-col :span="6">
+        <el-card class="card-left">
+            <div slot="header">
+              <div class="left_v">
+                <span>地区列表</span>
+              </div>
+            </div>
+            <div v-for="o in 4" :key="o">
+              {{'列表内容 ' + o }}
+            </div>
+          </el-card>
+      </el-col>
+      <el-col :span="18">
+        <el-card class="card-rigth">
+            <div slot="header">
+              <span>地区列表</span>
+            </div>
+            <div v-for="o in 4" :key="o">
+              {{'列表内容 ' + o }}
+            </div>
+          </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -8,6 +33,17 @@ export default {
 }
 </script>
 
-<style>
 
+<style scoped lang='scss'>
+.card-rigth{
+  margin: 0 15px 15px;
+}
+.card-left{
+  margin-left: 15px;
+  margin-bottom: 15px;
+}
+.left_v{
+  border-left: 10px;
+  border-color: red;
+}
 </style>
