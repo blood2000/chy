f24e2839034af47434d128af0c238a59c5954ddd
huangst <313949553@qq.com>
2021-03-24 15:50:21 +0800

    优化

--- src/api/enterprise/company/address.js
+++ src/api/enterprise/company/address.js
@@ -4,8 +4,8 @@ import request from '@/utils/request';
 export function listAddress(query) {
   return request({
     url: '/assets/shipment/address/list',
-    method: 'get',
-    params: query
+    method: 'post',
+    data: query
   });
 }
 
