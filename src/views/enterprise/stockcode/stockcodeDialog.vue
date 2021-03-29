c72688ad6c70b6f10455faa8360d28fb10d35683
tyuxiu <tyx19921018>
2021-03-29 10:47:02 +0800

    修改

--- src/views/enterprise/stockcode/stockcodeDialog.vue
+++ src/views/enterprise/stockcode/stockcodeDialog.vue
@@ -115,7 +115,7 @@ export default {
     generateCode() {
       generateCode({ id: this.form.id }).then(response => {
         this.msgSuccess(response.msg);
-        this.form.cargoCodeQR = response.data.msg;
+        this.form.cargoCodeQR = response.data.data;
         this.$forceUpdate();
       });
     }
