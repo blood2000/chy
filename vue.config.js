f4113afac62964b600d7ca5e4b66ce688a679b2b
fanrk <442189385@qq.com>
2021-04-10 08:55:14 +0800

    添加立志IP

--- vue.config.js
+++ vue.config.js
@@ -36,9 +36,10 @@ module.exports = {
       [process.env.VUE_APP_BASE_API]: {
         // target: `http://10.0.0.75:8080`, // 测试
         // target: `http://192.168.30.1:8080`, // 绍
-        target: `http://192.168.30.171:8080`, // 觉
+        // target: `http://192.168.30.171:8080`, // 觉
         // target: `http://192.168.30.208:8080`, //秀
         // target: `http://192.168.30.186:8080`, //同
+        target: `http://192.168.30.134:8080`, //志
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
