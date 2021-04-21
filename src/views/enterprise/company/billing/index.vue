a16a4403143e06a2b633400a90ed3668d72f117c
huangst <313949553@qq.com>
2021-04-21 20:55:48 +0800

    修改样式

--- src/views/enterprise/company/billing/index.vue
+++ src/views/enterprise/company/billing/index.vue
@@ -18,7 +18,7 @@
     <div class="app-container">
       <h3 class="g-title-medium mb10">开票信息</h3>
       <el-form ref="form" :model="form" :rules="rules" label-width="140px" :label-position="'left'">
-        <h5 class="g-card-title g-strong mb20">增值税发票开票信息</h5>
+        <h5 class="g-card-title mb20">增值税发票开票信息</h5>
         <!-- <el-form-item label="发票编码" prop="code">
           <el-input v-model="form.code" placeholder="请输入发票编码" class="input-width" clearable />
         </el-form-item>
@@ -43,7 +43,7 @@
         </el-form-item>
         <hr class="g-hr">
 
-        <h5 class="g-card-title g-strong mb20">发票收票地址</h5>
+        <h5 class="g-card-title mb20">发票收票地址</h5>
         <el-form-item label="收票人姓名" prop="payeeName">
           <el-input v-model="form.payeeName" placeholder="请输入收票人姓名" class="input-width" clearable />
         </el-form-item>
