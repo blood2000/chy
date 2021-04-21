aa83a212eb9c38a7a559591ba7299d022c4142f3
huangst <313949553@qq.com>
2021-04-21 16:18:13 +0800

    表格按钮溢出。。。组件

---
+++ src/components/Dropdown/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- 表格按钮超出显示...，hover后下拉展示 -->
+  <el-dropdown class="ml10" trigger="click" placement="bottom">
+    <el-button type="text" class="dropdown-button">
+      <span class="g-strong">···</span>
+    </el-button>
+    <el-dropdown-menu slot="dropdown" class="dropdown-menu">
+      <slot />
+    </el-dropdown-menu>
+  </el-dropdown>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+/* 自定义下拉样式 */
+.dropdown-button{
+  padding: 7px 0;
+}
+.dropdown-menu{
+  margin-top: 5px;
+}
+</style>
