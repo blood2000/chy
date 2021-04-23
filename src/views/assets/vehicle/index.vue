03728cc461d4bd310ff4a6cfef7f65c008afb1d8
huangst <313949553@qq.com>
2021-04-23 10:41:16 +0800

    全局按钮样式替换

--- src/views/assets/vehicle/index.vue
+++ src/views/assets/vehicle/index.vue
@@ -103,12 +103,14 @@
         </el-form-item>
         <el-form-item>
           <el-button
-            type="cyan"
+            type="primary"
             icon="el-icon-search"
             size="mini"
             @click="handleQuery"
           >搜索</el-button>
           <el-button
+            type="primary"
+            plain
             icon="el-icon-refresh"
             size="mini"
             @click="resetQuery"
