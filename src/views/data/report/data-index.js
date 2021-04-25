eca325b1fc40beea87d885ff5aad90c7893d83bf
chenhj <121459108@qq.com>
2021-04-25 09:11:12 +0800

    核算-修改bug

---
+++ src/views/data/report/data-index.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    prop: 'edit',
+    isShow: true,
+    label: '操作',
+    width: 180,
+    fixed: 'right'
+  }
+];
+
+// redisAddressList	装货卸货地址信息列表	array	货源商品装货卸货地址_3
+// redisOrderClassGoodsVoList	货集码列表	array	货源（分类）码编号_2
+// redisOrderFreightInfoVoList	运费规则商品下的地址下的规则	array	RedisOrderFreightInfoVo
+// redisOrderGoodsVoList		array	货源商品信息_2
+// redisOrderSpecifiedVoList		array	货源指定接单人表_1
