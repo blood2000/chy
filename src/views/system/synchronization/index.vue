b6c8b69b1f53c61224c73a76006cbe8b36209585
huangst <313949553@qq.com>
2021-04-25 09:50:16 +0800

    同步管理

---
+++ src/views/system/synchronization/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="app-container">
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <el-card class="box-card" shadow="hover">
+            <div @click="setShipment">货主信息同步redis</div>
+          </el-card>
+        </el-col>
+        <el-col :span="6">
+          <el-card class="box-card" shadow="hover">
+            <div @click="setUser">用户信息同步redis</div>
+          </el-card>
+        </el-col>
+        <el-col :span="6">
+          <el-card class="box-card" shadow="hover">
+            <div @click="setDriver">司机信息同步redis</div>
+          </el-card>
+        </el-col>
+        <el-col :span="6">
+          <el-card class="box-card" shadow="hover">
+            <div @click="setTeam">调度者信息同步redis</div>
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+import { setShipmentInfoRedisAll, setUserRoleInfoRedisAll, setDriverRedisAll, setTeamrRedisAll } from '@/api/system/synchronization';
+
+export default {
+  name: 'Synchronization',
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+    setShipment() {
+      setShipmentInfoRedisAll().then(() => {
+        this.msgSuccess('同步成功');
+      });
+    },
+    setUser() {
+      setUserRoleInfoRedisAll().then(() => {
+        this.msgSuccess('同步成功');
+      });
+    },
+    setDriver() {
+      setDriverRedisAll().then(() => {
+        this.msgSuccess('同步成功');
+      });
+    },
+    setTeam() {
+      setTeamrRedisAll().then(() => {
+        this.msgSuccess('同步成功');
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.box-card{
+  cursor: default;
+  ::v-deep.el-card__body{
+    padding: 0;
+    >div{
+      padding: 15px 20px 20px 20px;
+    }
+  }
+}
+</style>
