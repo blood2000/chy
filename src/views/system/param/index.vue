29e01a7b276eb1e751e3f1775496c0ae752c5d0d
huangst <313949553@qq.com>
2021-04-26 18:34:33 +0800

    业务参数

--- src/views/system/param/index.vue
+++ src/views/system/param/index.vue
@@ -17,61 +17,121 @@
         </div>
       </el-col>
       <el-col :lg="19" :md="18" :sm="17" :xs="24">
-        <div v-for="item in formList" :key="item.id" class="app-container app-container--card">
-          <h3 class="g-card-title g-card-header mb20">这是标题</h3>
-        </div>
+        <template v-if="formList.length > 0">
+          <div v-for="item in formList" :key="item.id" class="app-container app-container--card">
+            <h3 class="g-card-title g-card-header mb20">
+              {{ item.label }}
+              <i class="el-icon-plus g-color-blue ml5" style="padding: 14px 5px; cursor: pointer" @click="addParam(item)" />
+              <i class="el-icon-close g-color-gray fr" style="padding: 14px 5px; cursor: pointer" @click="closeCard(item)" />
+            </h3>
+            <div v-if="item.paramList.length > 0">
+              <el-form label-width="140px" label-position="left">
+                <el-form-item v-for="formItem in item.paramList" :key="formItem.code" :label="formItem.paramCnName" :prop="formItem.paramEnName">
+                  <!-- input -->
+                  <template v-if="formItem.paramFormType === '1'">
+                    <el-input v-model="formItem.paramValue" disabled class="input-width" />
+                  </template>
+                  <!-- textarea -->
+                  <template v-else-if="formItem.paramFormType === '3'">
+                    <el-input v-model="formItem.paramValue" type="textarea" disabled class="input-width" />
+                  </template>
+                  <!-- select -->
+                  <template v-else-if="formItem.paramFormType === '4'">
+                    <el-select v-model="formItem.paramValue" disabled class="input-width">
+                      <el-option
+                        v-for="dict in formItem.paramDicts"
+                        :key="dict.dictValue"
+                        :label="dict.dictLabel"
+                        :value="dict.dictValue"
+                      />
+                    </el-select>
+                  </template>
+                  <!-- radio -->
+                  <template v-else-if="formItem.paramFormType === '5'">
+                    <el-radio-group v-model="formItem.paramValue" disabled>
+                      <el-radio
+                        v-for="dict in formItem.paramDicts"
+                        :key="dict.dictValue"
+                        :label="dict.dictValue"
+                      >{{ dict.dictLabel }}</el-radio>
+                    </el-radio-group>
+                  </template>
+                  <!-- checkbox -->
+                  <template v-else-if="formItem.paramFormType === '6'">
+                    <el-checkbox v-model="formItem.paramValue" disabled label="是否勾选" />
+                  </template>
+                  <!-- switch -->
+                  <template v-else-if="formItem.paramFormType === '7'">
+                    <el-switch v-model="formItem.paramValue" disabled />
+                  </template>
+                  <!-- input -->
+                  <template v-else>
+                    <el-input v-model="formItem.paramValue" disabled class="input-width" />
+                  </template>
+                  <!-- button -->
+                  <el-button type="text" class="ml10" @click="editParam(item, formItem)"><i class="el-icon-edit-outline" /></el-button>
+                  <el-button type="text" class="ml10" @click="deleteParam(formItem)"><i class="el-icon-delete" /></el-button>
+                </el-form-item>
+              </el-form>
+            </div>
+            <div v-else class="none-card">
+              <i class="el-icon-document-remove g-color-blue" />
+              暂无数据
+            </div>
+          </div>
+        </template>
+        <template v-else>
+          <div class="app-container">
+            <div class="none-card">
+              <i class="el-icon-document-remove g-color-blue" />
+              暂无数据
+            </div>
+          </div>
+        </template>
       </el-col>
     </el-row>
+
+    <!-- 新增/编辑树 对话框 -->
+    <setting-dialog ref="settingDialogRef" :open.sync="settingOpen" :title="title" @refresh="getTree" />
+    <!-- 新增/编辑参数 对话框 -->
+    <param-dialog ref="paramDialogRef" :open.sync="paramOpen" :title="title" @refresh="refreshParam" />
   </div>
 </template>
 
 <script>
-// import {  } from '@/api/system/param';
+import { getParamTree, delParamTree, getParam, delParam } from '@/api/system/param';
+import SettingDialog from './settingDialog';
+import ParamDialog from './paramDialog.vue';
 
 export default {
   name: 'Param',
+  components: {
+    SettingDialog,
+    ParamDialog
+  },
   data() {
     return {
+      settingOpen: false,
+      paramOpen: false,
+      title: '',
       // 树
-      treeData: [{
-        id: 1,
-        label: '一级 1',
-        type: 'class',
-        children: [
-          {
-            id: 2,
-            label: '二级 1'
-          }, {
-            id: 3,
-            label: '二级 2'
-          }, {
-            id: 4,
-            label: '二级 3'
-          }
-        ]
-      }],
+      treeData: [],
       defaultTreeProps: {
         children: 'children',
-        label: 'label',
-        // 父节点不可选中
-        disabled: function(data, node) {
-          if (data.type === 'class') {
-            return !data.leaf;
-          }
-        }
+        label: 'label'
       },
       // 表单
-      formList: [
-        { id: 1 },
-        { id: 2 }
-      ]
+      formList: []
     };
+  },
+  created() {
+    this.getTree();
   },
   methods: {
     renderContent(h, { node, data, store }) {
       return (
         <span class='custom-tree-node'>
-          <span>{node.label}</span>
+          <span class='custom-tree-text'>{node.label}</span>
           <span class='custom-tree-button'>
             <el-button type='text' on-click={ () => this.appendTree(data) }><i class='el-icon-circle-plus-outline' /></el-button>
             <el-button type='text' on-click={ () => this.editTree(node, data) }><i class='el-icon-edit-outline' /></el-button>
@@ -79,30 +139,108 @@ export default {
           </span>
         </span>);
     },
-    appendTree(data) {
-      const newChild = { label: 'testtest', children: [] };
-      if (!data.children) {
-        this.$set(data, 'children', []);
-      }
-      data.children.push(newChild);
+    getTree() {
+      getParamTree().then(response => {
+        this.treeData = response.data || [];
+        if (this.treeData && this.treeData.length > 0 && this.treeData[0].children && this.treeData[0].children.length > 0) {
+          this.handleNodeClick(this.treeData[0].children[0]);
+        }
+      });
     },
-    editTree(data) {
-      console.log(data);
+    appendTree(data) {
+      this.$refs.settingDialogRef.reset();
+      this.title = '新增';
+      this.$refs.settingDialogRef.setAddForm(data);
+      this.settingOpen = true;
+    },
+    editTree(node, data) {
+      this.$refs.settingDialogRef.reset();
+      this.title = '编辑';
+      this.$refs.settingDialogRef.setEditForm(data);
+      this.settingOpen = true;
     },
     removeTree(node, data) {
-      const parent = node.parent;
-      const children = parent.data.children || parent.data;
-      const index = children.findIndex(d => d.id === data.id);
-      children.splice(index, 1);
+      this.$confirm('是否确认删除配置名称为"' + data.label + '"的数据项?', '警告', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(function() {
+        return delParamTree(data.code);
+      }).then(() => {
+        this.getTree();
+        this.msgSuccess('删除成功');
+      });
     },
     handleNodeClick(data) {
-      console.log(data);
+      // 父节点不可选中
+      if (data.id === 0) return;
+      // 选中参数
+      const flag = this.formList.some(el => {
+        return el.id === data.id;
+      });
+      if (!flag) {
+        getParam({ typeCode: data.code }).then(response => {
+          data.paramList = [...(response.data || [])];
+          this.formList.unshift(data);
+        });
+      }
+    },
+    closeCard(data) {
+      this.formList = this.formList.filter(el => {
+        return el.id !== data.id;
+      });
+    },
+    addParam(data) {
+      this.$refs.paramDialogRef.reset();
+      this.title = '新增';
+      this.$refs.paramDialogRef.setAddForm(data);
+      this.paramOpen = true;
+    },
+    editParam(data, param) {
+      this.$refs.paramDialogRef.reset();
+      this.title = '编辑';
+      this.$refs.paramDialogRef.setEditForm(data, param);
+      this.paramOpen = true;
+    },
+    deleteParam(param) {
+      this.$confirm('是否确认删除参数名称为"' + param.paramCnName + '"的数据项?', '警告', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(function() {
+        return delParam(param.code);
+      }).then(() => {
+        this.getTree();
+        this.msgSuccess('删除成功');
+      });
+    },
+    refreshParam(typeCode) {
+      getParam({ typeCode: typeCode }).then(response => {
+        this.formList.forEach((el, index) => {
+          if (el.code === typeCode) {
+            this.formList[index].paramList = [...(response.data || [])];
+            this.$forceUpdate();
+          }
+        });
+      });
     }
   }
 };
 </script>
 
 <style lang="scss">
+// 表单样式
+.input-width{
+  width: 40%;
+  min-width: 300px;
+}
+// 暂无数据样式
+.none-card{
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+}
+// 树样式
 .page-param-tree{
   .el-tree-node__content .custom-tree-node{
     width: 100%;
@@ -113,6 +251,12 @@ export default {
     padding-right: 8px;
     .custom-tree-button{
       display: none;
+      >button{
+        padding: 5px;
+        +.el-button{
+          margin-left: 0;
+        }
+      }
     }
     &:hover{
       .custom-tree-button{
