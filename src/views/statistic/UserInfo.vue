1259a1ba89ea8f6f57e240ef09d0b3e5553a427c
huangst <313949553@qq.com>
2021-04-26 22:01:31 +0800

    数据看板

---
+++ src/views/statistic/UserInfo.vue
@@ -0,0 +1,119 @@
+<template>
+  <!-- 用户情况 -->
+  <div class="container ly-flex-pack-justify">
+    <div class="container__box ly-flex-pack-justify ly-flex-v">
+      <div class="container__box__content">
+        <p class="label">总用户数(万)</p>
+        <p class="text">0.211</p>
+      </div>
+      <div class="container__box__content">
+        <p class="label">今日新增用户</p>
+        <p class="text blod">
+          0.211
+          <img src="@/assets/images/statistic/arow_up.png">
+          <span>12.6%</span>
+        </p>
+      </div>
+    </div>
+    <div class="container__box ly-flex-pack-justify ly-flex-v">
+      <div class="container__box__content">
+        <p class="label">总货主数(万)</p>
+        <p class="text">0.211</p>
+      </div>
+      <div class="container__box__content">
+        <p class="label">今日新增货主</p>
+        <p class="text blod">
+          0.211
+          <img src="@/assets/images/statistic/arow_up.png">
+          <span>12.6%</span>
+        </p>
+      </div>
+    </div>
+    <div class="container__box ly-flex-pack-justify ly-flex-v">
+      <div class="container__box__content">
+        <p class="label">总调度者(万)</p>
+        <p class="text">0.211</p>
+      </div>
+      <div class="container__box__content">
+        <p class="label">今日新增调度者</p>
+        <p class="text blod">
+          0.211
+          <img src="@/assets/images/statistic/arow_up.png">
+          <span>12.6%</span>
+        </p>
+      </div>
+    </div>
+    <div class="container__box ly-flex-pack-justify ly-flex-v">
+      <div class="container__box__content">
+        <p class="label">总司机(万)</p>
+        <p class="text">0.211</p>
+      </div>
+      <div class="container__box__content">
+        <p class="label">今日新增司机</p>
+        <p class="text blod">
+          0.211
+          <img src="@/assets/images/statistic/arow_up.png">
+          <span>12.6%</span>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+.container{
+  height: calc(100% - 2.9rem);
+  &__box{
+    width: 25%;
+    height: 100%;
+    padding-left: 1.59rem;
+    position: relative;
+    &::before{
+      content: '';
+      position: absolute;
+      left: 0;
+      top: 0;
+      bottom: 0;
+      width: 1px;
+      background: linear-gradient(rgba(1, 227, 255, 0), rgba(1, 227, 255, 0.22), rgba(1, 227, 255, 0));
+    }
+    &__content{
+      width: 100%;
+      height: 50%;
+      >.label{
+        font-size: 0.6rem;
+        font-weight: 200;
+        color: #9CAFD0;
+        line-height: 1rem;
+        opacity: 0.7;
+      }
+      >.text{
+        font-size: 1.1rem;
+        font-weight: normal;
+        color: #FFFFFF;
+        line-height: 1.3rem;
+        opacity: 0.9;
+        &.blod{
+          font-weight: 700;
+          opacity: 1;
+        }
+        >img{
+          width: 0.9rem;
+          height: 0.6rem;
+        }
+        >span{
+          font-size: 0.6rem;
+          color: rgba(0, 210, 255, 1);
+          vertical-align: top;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/statistic/components/title.vue
+++ src/views/statistic/components/title.vue
@@ -1,44 +1,102 @@
 <template>
   <div class="title ly-flex">
-    <div class="before" />
+    <div class="before">
+      <img :src="require('@/assets/images/statistic/title_icon_'+icon+'.png')">
+    </div>
     <div class="text ly-flex-1">
       <slot />
     </div>
+    <div class="top_line" />
   </div>
 </template>
 
 <script>
 export default {
-  prop: {
+  props: {
     icon: {
       type: String,
-      default: null
+      default: '1'
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.title {
+.title{
   height: 1.9rem;
   margin-bottom: 1rem;
-  background: rgba(44, 62, 90, 0.5);
-  color: #01e3ff;
+  position: relative;
   >.before{
     width: 1.9rem;
     height: 1.9rem;
-    background: rgba(65, 99, 153, 0.3);
+    >img{
+      width: 1.4rem;
+      height: 1.4rem;
+      margin: 0.2rem 0;
+    }
   }
   >.text{
     line-height: 1.9rem;
-    padding-left: 0.6rem;
     font-size: 0.9rem;
     font-weight: bold;
+    color: #01e3ff;
     >span{
-      font-size: 0.8rem;
+      font-size: 0.7rem;
       font-weight: normal;
       padding-left: 0.4rem;
+      color: #3AA7BC;
     }
+  }
+  &::before{
+    content: '';
+    width: 1.2rem;
+    height: 1px;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    background: linear-gradient(to right, #1a88b1 0.6rem, rgba(50, 255, 255, 1) 0);
+  }
+  &::after{
+    content: '';
+    height: 1px;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: linear-gradient(to right, rgba(50, 255, 255, 0), rgba(79, 131, 131, 0.2));
+  }
+  &.title_1, &.title_2, &.title_3, &.title_4, &.title_5{
+    .top_line{
+      position: absolute;
+      top: 0;
+      right: 0;
+      height: 0.6rem;
+    }
+  }
+  &.title_1 .top_line{
+    width: 19.25rem;
+    background: url('~@/assets/images/statistic/title_1.png');
+    background-size: 100% 100%;
+  }
+  &.title_2 .top_line{
+    width: 19.25rem;
+    background: url('~@/assets/images/statistic/title_2.png');
+    background-size: 100% 100%;
+  }
+  &.title_3 .top_line{
+    width: 39.7rem;
+    background: url('~@/assets/images/statistic/title_3.png');
+    background-size: 100% 100%;
+  }
+  &.title_4 .top_line{
+    width: 9.35rem;
+    background: url('~@/assets/images/statistic/title_4.png');
+    background-size: 100% 100%;
+  }
+  &.title_5 .top_line{
+    width: 9.35rem;
+    background: url('~@/assets/images/statistic/title_5.png');
+    background-size: 100% 100%;
   }
 }
 </style>
