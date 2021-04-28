ee7fe13c6368e7f878732badd9f24b1eca103c4b
chenhj <121459108@qq.com>
2021-04-28 10:59:39 +0800

    修改样式

--- src/views/enterprise/project/projectDialog.vue
+++ src/views/enterprise/project/projectDialog.vue
@@ -213,7 +213,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 	.mr3{
 	  margin-right: 3%;
 	}
@@ -227,6 +227,6 @@ export default {
 	  text-align: left;
 	}
   .el-radio{
-    line-height: 36px !important;
+    line-height: 36px;
   }
 </style>
