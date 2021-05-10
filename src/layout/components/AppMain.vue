756d1dffb3403db0e65ee86f0e6af48bd1010291
huangst <313949553@qq.com>
2021-05-10 15:07:32 +0800

    快捷入口

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -24,8 +24,8 @@ export default {
 
 <style lang="scss" scoped>
 .app-main {
-  /* 50= navbar  50  */
-  min-height: calc(100vh - 50px);
+  /* 70 = navbar  70  */
+  min-height: calc(100vh - 70px);
   width: 100%;
   position: relative;
   overflow: hidden;
@@ -34,13 +34,13 @@ export default {
 
 .hasTagsView {
   .app-main {
-    /* 84 = navbar + tags-view = 50 + 34 */
-    min-height: calc(100vh - 84px);
+    /* 84 = navbar + tags-view = 70 + 34 */
+    min-height: calc(100vh - 104px);
   }
 }
 
 .fixed-header+.app-main{
-  height: calc(100vh - 84px);
+  height: calc(100vh - 104px);
   overflow-y: auto;
 }
 </style>
