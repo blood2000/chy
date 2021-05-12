676d3a1fc7aa648247c2828e29a74a91044d05b4
tyuxiu <tyx19921018>
2021-05-12 14:36:25 +0800

    钱包-交易记录修改完善

--- src/views/wallet/transactionRecord/index.vue
+++ src/views/wallet/transactionRecord/index.vue
@@ -46,26 +46,83 @@
         <el-tab-pane label="冻结记录" name="dj" />
         <el-tab-pane label="付款记录" name="fk" />
       </el-tabs>
-
-      <el-table v-loading="loading" stripe border :data="dataList">
+      <el-table v-loading="loading" stripe border :data="dataList" v-show="activeTab == 'dj'">
         <el-table-column label="运单号" align="center" prop="waybillNo" width="150" />
-        <el-table-column label="装货地" align="center" prop="" width="150" />
-        <el-table-column label="卸货地" align="center" prop="" width="150" />
-        <el-table-column label="货物大类" align="center" prop="" />
-        <el-table-column label="承运司机" align="center" prop="payeeName" />
+        <el-table-column label="装货地" align="center" prop="unloadAddress" width="150" />
+        <el-table-column label="卸货地" align="center" prop="loadAddress" width="150" />
+        <el-table-column label="货物大类" align="center" prop="goodsBigType" >
+          <template slot-scope="scope">
+            <span>{{ selectDictLabel(commodityCategoryCodeOptions, scope.row.goodsBigType) }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="货物小类" align="center" prop="goodsType" >
+          <template slot-scope="scope">
+            <span>{{ selectDictLabel(commoditySubclassOptions, scope.row.goodsType) }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="承运司机" align="center" prop="driverName" />
         <el-table-column label="承运车辆" align="center" prop="licenseNumber" />
         <el-table-column label="所属调度者" align="center" prop="teamName" width="150" />
-        <el-table-column label="交易类型" align="center" prop="" />
-        <el-table-column label="单价（元）" align="center" prop="" />
-        <el-table-column label="重量（吨）" align="center" prop="" />
-        <el-table-column label="总额（元）" align="center" prop="amount">
+        <!-- 付款方式：0-现金支付， 1-京东支付 2-交通银行， 3-新生支付，4-工商银行,5-传化支付,6-建行支付,7-环迅-->
+        <el-table-column label="交易类型" align="center" prop="payBy" >
+          <template>
+            <span>冻结</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="装货重量" align="center" prop="loadWeight" />
+        <el-table-column label="卸货重量" align="center" prop="unloadWeight" />
+        <el-table-column label="货物损耗(kg)" align="center" prop="wastage" />
+        <el-table-column label="货物单价（元）" align="center" prop="goodsPrice" />
+        <el-table-column label="成交单价（元）" align="center" prop="freightPriceDriver" />
+        <el-table-column label="冻结总额（元）" align="center" prop="freezeMoney"/>
+        <el-table-column label="操作时间" align="center" prop="updateTime" width="180" >
+          <template slot-scope="scope">
+            <span>{{ parseTime(scope.row.updateTime) }}</span>
+          </template>
+        </el-table-column>
+      </el-table>
+
+
+      <el-table v-loading="loading" stripe border :data="dataList" v-show="activeTab == 'fk'">
+        <el-table-column label="运单号" align="center" prop="waybillNo" width="150" />
+        <el-table-column label="装货地" align="center" prop="unloadAddress" width="150" />
+        <el-table-column label="卸货地" align="center" prop="loadAddress" width="150" />
+        <el-table-column label="货物大类" align="center" prop="goodsBigType" >
+          <template slot-scope="scope">
+            <span>{{ selectDictLabel(commodityCategoryCodeOptions, scope.row.goodsBigType) }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="货物小类" align="center" prop="goodsType" >
+          <template slot-scope="scope">
+            <span>{{ selectDictLabel(commoditySubclassOptions, scope.row.goodsType) }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="承运司机" align="center" prop="driverName" />
+        <el-table-column label="承运车辆" align="center" prop="licenseNumber" />
+        <el-table-column label="所属调度者" align="center" prop="teamName" width="150" />
+       <!-- 付款方式：0-现金支付， 1-京东支付 2-交通银行， 3-新生支付，4-工商银行,5-传化支付,6-建行支付,7-环迅-->
+        <el-table-column label="交易类型" align="center" prop="payBy" >
+          <template>
+            <span>付款</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="装货重量" align="center" prop="loadWeight" />
+        <el-table-column label="卸货重量" align="center" prop="unloadWeight" />
+        <el-table-column label="货物损耗(kg)" align="center" prop="wastage" />
+        <el-table-column label="货物单价（元）" align="center" prop="goodsPrice" />
+        <el-table-column label="成交单价（元）" align="center" prop="freightPriceDriver" />
+        <el-table-column label="实付金额（元）" align="center" prop="amount">
           <!-- 金额：单位分转为元 -->
           <template slot-scope="scope">
             <span>{{ scope.row.amount ? (scope.row.amount/100).toFixed(2) : scope.row.amount }}</span>
           </template>
         <!-- 付款类型 -->
         </el-table-column>
-        <el-table-column label="操作时间" align="center" prop="payTime" width="180" />
+        <el-table-column label="操作时间" align="center" prop="updateTime" width="180" >
+          <template slot-scope="scope">
+            <span>{{ parseTime(scope.row.updateTime) }}</span>
+          </template>
+        </el-table-column>
       </el-table>
 
       <pagination
@@ -102,8 +159,20 @@ export default {
       typeOptions: [
         { dictLabel: '全部', dictValue: 0 }
       ],
+      // 付款方式：0-现金支付， 1-京东支付 2-交通银行， 3-新生支付，4-工商银行,5-传化支付,6-建行支付,7-环迅
+      payByOptions: [
+        { dictLabel: '现金支付', dictValue: 0 },
+        { dictLabel: '京东支付', dictValue: 1 },
+        { dictLabel: '交通银行', dictValue: 2 },
+        { dictLabel: '新生支付', dictValue: 3 },
+        { dictLabel: '工商银行', dictValue: 4 },
+        { dictLabel: '传化支付', dictValue: 5 },
+        { dictLabel: '建行支付', dictValue: 6 },
+        { dictLabel: '环迅', dictValue: 7 }
+      ],
       // 查询参数
       queryParams: {
+        usedFreeze: 1,
         pageNum: 1,
         pageSize: 10,
         updateTimeBegin: undefined,
@@ -111,16 +180,40 @@ export default {
       },
       activeName: '近三月',
       updateTimeBegin: undefined,
-      updateTimeEnd: undefined
+      updateTimeEnd: undefined,
+      // 大类字典类型
+      commodityCategory: {
+        'dictPid': '0',
+        'dictType': 'goodsType'
+      },
+      commodityCategoryCodeOptions: [],
+      // 小类字典类型
+      commoditySubclass: {
+        'dictPid': '',
+        'dictType': 'goodsType'
+      },
+      commoditySubclassOptions: []
     };
   },
   created() {
     this.changeTimeFormate();
     this.getList();
+    this.listByDict(this.commodityCategory).then(response => {
+      this.commodityCategoryCodeOptions = response.data;
+    });
+    this.listByDict(this.commoditySubclass).then(response => {
+      this.commoditySubclassOptions = response.data;
+    });
   },
   methods: {
-    handleTabClick() {
-      // console.log(this.activeTab);
+    handleTabClick(e) {
+      this.activeTab = e.name;
+      if (this.activeTab === 'dj') {
+        this.queryParams.usedFreeze = 1;
+      } else {
+        this.queryParams.usedFreeze = 0;
+      }
+      this.getList();
     },
     /** 查询列表 */
     getList() {
