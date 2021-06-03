219956c932c192b0ffa6044f7af3ab8faa3846f4
huangst <313949553@qq.com>
2021-06-03 14:45:11 +0800

    个人中心样式

--- src/views/system/user/profile/index.vue
+++ src/views/system/user/profile/index.vue
@@ -1,60 +1,48 @@
 <template>
-  <div style="margin: 0 15px">
-    <el-row :gutter="20">
-      <el-col :span="6" :xs="24">
-        <el-card class="box-card">
-          <div slot="header" class="clearfix">
-            <span>个人信息</span>
-          </div>
-          <div>
-            <div class="text-center">
-              <userAvatar :user="user" @fresh="getUser" />
-            </div>
-            <ul class="list-group list-group-striped">
-              <li class="list-group-item">
-                <svg-icon icon-class="user" />用户姓名
-                <div class="pull-right">{{ user.nickName }}</div>
-              </li>
-              <li class="list-group-item">
-                <svg-icon icon-class="phone" />手机号码
-                <div class="pull-right">{{ user.phonenumber }}</div>
-              </li>
-              <li class="list-group-item">
-                <svg-icon icon-class="email" />用户邮箱
-                <div class="pull-right">{{ user.email }}</div>
-              </li>
-              <li class="list-group-item">
-                <svg-icon icon-class="tree" />所属组织
-                <div v-if="user.org" class="pull-right">{{ user.org.orgName }}</div>
-              </li>
-              <li class="list-group-item">
-                <svg-icon icon-class="peoples" />所属角色
-                <div class="pull-right">{{ roleGroup }}</div>
-              </li>
-              <li class="list-group-item">
-                <svg-icon icon-class="date" />创建日期
-                <div class="pull-right">{{ user.createTime }}</div>
-              </li>
-            </ul>
-          </div>
-        </el-card>
-      </el-col>
-      <el-col :span="18" :xs="24">
-        <el-card>
-          <div slot="header" class="clearfix">
-            <span>基本资料</span>
-          </div>
-          <el-tabs v-model="activeTab">
-            <el-tab-pane label="基本资料" name="userinfo">
-              <userInfo :user="user" />
-            </el-tab-pane>
-            <el-tab-pane label="修改密码" name="resetPwd">
-              <resetPwd :user="user" />
-            </el-tab-pane>
-          </el-tabs>
-        </el-card>
-      </el-col>
-    </el-row>
+  <div class="profile-index ly-flex ly-flex-pack-justify">
+    <div class="left-panel app-container">
+      <div class="text-center">
+        <userAvatar :user="user" @fresh="getUser" />
+      </div>
+      <div class="text-center left-panel-name">
+        {{ user.nickName }}
+      </div>
+      <div v-if="user.org.orgName" class="text-center left-panel-org">
+        {{ user.org.orgName }}
+      </div>
+      <ul class="list-group list-group-striped">
+        <li class="list-group-item">
+          手机号码
+          <div class="pull-right">{{ user.phonenumber }}</div>
+        </li>
+        <li class="list-group-item">
+          用户邮箱
+          <div class="pull-right">{{ user.email }}</div>
+        </li>
+        <li class="list-group-item">
+          所属组织
+          <div v-if="user.org" class="pull-right">{{ user.org.orgName }}</div>
+        </li>
+        <li class="list-group-item">
+          所属角色
+          <div class="pull-right">{{ roleGroup }}</div>
+        </li>
+        <li class="list-group-item">
+          创建日期
+          <div class="pull-right">{{ user.createTime }}</div>
+        </li>
+      </ul>
+    </div>
+    <div class="right-panel app-container ly-flex-1">
+      <el-tabs v-model="activeTab">
+        <el-tab-pane label="基本资料" name="userinfo">
+          <userInfo :user="user" />
+        </el-tab-pane>
+        <el-tab-pane label="修改密码" name="resetPwd">
+          <resetPwd :user="user" />
+        </el-tab-pane>
+      </el-tabs>
+    </div>
   </div>
 </template>
 
@@ -89,3 +77,37 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.profile-index{
+  height: calc(100% - 15px);
+  flex-wrap: wrap;
+  margin: 0 0 15px 15px;
+  >.left-panel{
+    width: 368px;
+    min-height: 100%;
+    margin: 0 15px 15px 0;
+    .left-panel-name{
+      font-size: 18px;
+      font-family: PingFang SC;
+      font-weight: bold;
+      line-height: 22px;
+      color: #262626;
+    }
+    .left-panel-org{
+      font-size: 14px;
+      font-family: PingFang SC;
+      font-weight: 500;
+      line-height: 22px;
+      color: #909398;
+      margin-top: 3px;
+    }
+  }
+  >.right-panel{
+    width: calc(100% - 398px);
+    height: 370px;
+    min-width: 368px;
+    margin: 0 15px 0 0;
+  }
+}
+</style>
--- src/views/system/user/profile/resetPwd.vue
+++ src/views/system/user/profile/resetPwd.vue
@@ -1,17 +1,17 @@
 <template>
   <el-form ref="form" :model="user" :rules="rules" label-width="80px">
     <el-form-item label="旧密码" prop="oldPassword">
-      <el-input v-model="user.oldPassword" placeholder="请输入旧密码" type="password" />
+      <el-input v-model="user.oldPassword" placeholder="请输入旧密码" type="password" class="input-width" />
     </el-form-item>
     <el-form-item label="新密码" prop="newPassword">
-      <el-input v-model="user.newPassword" placeholder="请输入新密码" type="password" />
+      <el-input v-model="user.newPassword" placeholder="请输入新密码" type="password" class="input-width" />
     </el-form-item>
     <el-form-item label="确认密码" prop="confirmPassword">
-      <el-input v-model="user.confirmPassword" placeholder="请确认密码" type="password" />
+      <el-input v-model="user.confirmPassword" placeholder="请确认密码" type="password" class="input-width" />
     </el-form-item>
     <el-form-item>
-      <el-button type="primary" size="mini" @click="submit">保存</el-button>
-      <el-button type="danger" size="mini" @click="close">关闭</el-button>
+      <el-button class="submit-button" type="primary" @click="submit">保 存</el-button>
+      <!-- <el-button type="danger" size="mini" @click="close">关闭</el-button> -->
     </el-form-item>
   </el-form>
 </template>
@@ -70,3 +70,14 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.submit-button{
+  height: 30px;
+  padding: 7px 20px;
+  border-radius: 2px;
+}
+.input-width{
+  width: 300px;
+}
+</style>
