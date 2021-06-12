c05038d4e4a50e3e5af96541603d749ee2f782aa
huangst <313949553@qq.com>
2021-06-12 10:19:07 +0800

    接口超时和异常不报错

--- src/utils/request.js
+++ src/utils/request.js
@@ -121,23 +121,23 @@ service.interceptors.response.use(res => {
 },
 error => {
   console.log('err' + error);
-  let { message } = error;
+  const { message } = error;
   if (message === 'Network Error') {
-    message = '后端接口连接异常';
+    // message = '后端接口连接异常';
   } else if (message.includes('timeout')) {
-    message = '系统接口请求超时';
+    // message = '系统接口请求超时';
   } else if (message.includes('Request failed with status code')) {
-    message = '系统接口' + message.substr(message.length - 3) + '异常';
+    // message = '系统接口' + message.substr(message.length - 3) + '异常';
+  } else {
+    Message({
+      message: message,
+      type: 'error',
+      duration: 5 * 1000,
+      showClose: true
+    });
   }
-  Message({
-    message: message,
-    type: 'error',
-    duration: 5 * 1000,
-    showClose: true
-  });
   return Promise.reject(error);
-}
-);
+});
 
 // 通用下载方法
 export function download(url, params, filename, headers) {
