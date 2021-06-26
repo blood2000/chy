00d27af17796509ce9a8a245fb662c594da968b1
chenhj <121459108@qq.com>
2021-06-26 14:13:19 +0800

    批量上报

--- src/api/data/report.js
+++ src/api/data/report.js
@@ -15,65 +15,65 @@ export function waybillReport(query) {
 // 1上报司机信息
 export function waybillReportDriver(waybillReportCode) {
   return request({
-    url: '/transportation/waybillReport/driver/' + waybillReportCode,
+    url: '/transportation/waybillReport/driver/' + waybillReportCode + '/-1',
     method: 'get'
   });
 }
 // 1根据司机编码上报司机信息
 export function waybillReportDriverByCode(driverCode) {
   return request({
-    url: '/transportation/waybillReport/driverByCode/' + driverCode,
+    url: '/transportation/waybillReport/driverByCode/' + driverCode + '/-1',
     method: 'get'
   });
 }
 // 2上报车辆信息
 export function waybillReportVehicle(waybillReportCode) {
   return request({
-    url: '/transportation/waybillReport/vehicle/' + waybillReportCode,
+    url: '/transportation/waybillReport/vehicle/' + waybillReportCode + '/-1',
     method: 'get'
   });
 }
 // 2根据车辆编码上报车辆信息
 export function waybillReportVehicleByCode(vehicleCode) {
   return request({
-    url: '/transportation/waybillReport/vehicleByCode/' + vehicleCode,
+    url: '/transportation/waybillReport/vehicleByCode/' + vehicleCode + '/-1',
     method: 'get'
   });
 }
 // 3上报运单
 export function waybillReportWaybill(waybillReportCode) {
   return request({
-    url: '/transportation/waybillReport/waybill/' + waybillReportCode,
+    url: '/transportation/waybillReport/waybill/' + waybillReportCode + '/-1',
     method: 'get'
   });
 }
 // 4上报装货位置
 export function waybillReportLoad(waybillReportCode) {
   return request({
-    url: '/transportation/waybillReport/load/' + waybillReportCode,
+    url: '/transportation/waybillReport/load/' + waybillReportCode + '/-1',
     method: 'get'
   });
 }
 // 5上报卸货位置
 export function waybillReportUnload(waybillReportCode) {
   return request({
-    url: '/transportation/waybillReport/unload/' + waybillReportCode,
+    url: '/transportation/waybillReport/unload/' + waybillReportCode + '/-1',
     method: 'get'
   });
 }
 // 6上报资金流水
 export function waybillReportBill(waybillReportCode) {
   return request({
-    url: '/transportation/waybillReport/bill/' + waybillReportCode,
+    url: '/transportation/waybillReport/bill/' + waybillReportCode + '/-1',
     method: 'get'
   });
 }
 
 
-// 查询调度列表
+// 批量上报
 export function batch(data) {
   return request({
-    url: '/transportation/waybillReport/batch',
+    url: '/transportation/waybillReport/batch/-1',
     method: 'post',
     data: data
   });
