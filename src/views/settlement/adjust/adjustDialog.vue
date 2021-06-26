94043536f9fbd0bc03360d16b73168faeb8bc241
chenhj <121459108@qq.com>
2021-06-26 14:12:33 +0800

    是否

--- src/views/settlement/adjust/adjustDialog.vue
+++ src/views/settlement/adjust/adjustDialog.vue
@@ -1,159 +1,171 @@
 <template>
-  <el-dialog class="i-adjust" :title="title" :visible="visible" width="1400px" :close-on-click-modal="false" append-to-body @close="cancel">
 
-    <el-table v-loading="loading" highlight-current-row :data="adjustlist" border>
+  <div>
+    <el-dialog class="i-adjust" :title="title" :visible="visible" width="1400px" :close-on-click-modal="false" append-to-body @close="cancel">
 
-      <el-table-column width="160" label="运输单号" show-overflow-tooltip align="center" prop="waybillNo" />
+      <el-table v-loading="loading" highlight-current-row :data="adjustlist" border>
 
-      <el-table-column width="120" label="司机姓名" align="center" prop="driverName" />
-      <el-table-column width="120" label="司机电话" align="center" prop="driverPhone" />
-      <el-table-column width="120" label="车牌号" align="center" prop="licenseNumber" />
+        <el-table-column width="160" label="运输单号" show-overflow-tooltip align="center" prop="waybillNo" />
 
-      <el-table-column width="160" label="装货数量" align="left" prop="loadWeight">
-        <template slot-scope="scope">
-          <span v-if="scope.row.isDregs === 1">{{ scope.row.loadWeight }}</span>
-          <div v-else>
-            <el-input-number v-if="scope.row.stowageStatus !== '2'" v-model="scope.row.loadWeight" :controls="false" placeholder="请输入装货数量" style="width:100%;" size="mini" @blur="handlerBlur(scope.row)" />
-            <span v-else>{{ scope.row.loadWeight }}</span>
-          </div>
+        <el-table-column width="120" label="司机姓名" align="center" prop="driverName" />
+        <el-table-column width="120" label="司机电话" align="center" prop="driverPhone" />
+        <el-table-column width="120" label="车牌号" align="center" prop="licenseNumber" />
 
-        </template>
-      </el-table-column>
-      <el-table-column width="160" label="卸货数量" align="left" prop="unloadWeight">
-        <template slot-scope="scope">
-          <span v-if="scope.row.isDregs === 1">{{ scope.row.unloadWeight }}</span>
-          <div v-else>
-            <el-input-number v-if="scope.row.stowageStatus !== '2'" v-model="scope.row.unloadWeight" :controls="false" placeholder="请输入卸货数量" style="width:100%;" size="mini" @blur="handlerBlur(scope.row)" />
-            <span v-else>{{ scope.row.unloadWeight }}</span>
-          </div>
-        </template>
-      </el-table-column>
-
-      <el-table-column width="160" label="配载方式" align="center" prop="lossAllowScope">
-        <template slot-scope="scope">
-          <span v-show="scope.row.stowageStatus === '0'">
-            吨数配载
-          </span>
-          <span v-show="scope.row.stowageStatus === '1'">
-            方数配载
-          </span>
-          <span v-show="scope.row.stowageStatus === '2'">
-            车数配载
-          </span>
-        </template>
-      </el-table-column>
-
-      <el-table-column width="160" label="路耗(吨/方)" align="center" prop="loss">
-        <template slot-scope="scope">
-          <span v-if="scope.row.stowageStatus === '0'">{{ floor((scope.row.loss -0), 2) }}</span>
-          <span v-else>{{ scope.row.loss || 0 }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column width="160" label="路耗允许范围(吨/方)" align="center" prop="lossAllowScope">
-        <template slot-scope="scope">
-          <span>{{ scope.row.lossAllowScope? _lossAllowScope(scope.row.lossAllowScope, scope.row.stowageStatus === '0' ) : '--' }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column width="160" label="货物单价(元)" align="center" prop="goodsPrice" />
-
-      <el-table-column width="160" label="货主成交单价(元)" align="center" prop="freightPrice" />
-
-      <el-table-column width="160" label="司机成交单价(元)" align="center" prop="freightPriceDriver" />
-      <el-table-column width="160" label="亏涨扣费(元)" align="center" prop="lossDeductionFee" />
-
-
-      <el-table-column width="120" label="抹零金额(元)" align="center" prop="m0Fee" />
-
-      <el-table-column width="160" label="司机实收运费(元)" align="center" prop="deliveryFeePractical" />
-
-      <!-- 补贴项目 -->
-      <el-table-column align="center" width="420" label="补贴项目">
-        <template slot="header">
-          <span>补贴项目
-            <el-button type="text" @click="isEdit2 = !isEdit2"><i class="el-icon-edit" /></el-button>
-            <PopoverCom />
-          </span>
-
-        </template>
-        <template slot-scope="scope">
-          <span v-if="scope.row.isDregs == 1"> -- </span>
-
-          <el-form v-else :inline="true" label-position="right" size="mini" class="ly-flex" label-width="110px">
-            <div v-for="(freight, index) in scope.row.subsidiesFreightList" :key="index">
-              <!-- 渣土 其他不能修改 -->
-              <el-form-item :label="freight.cnName + '(元)'">
-                <span v-if="scope.row.isDregs === 1">{{ freight.ruleValue }}</span>
-                <div v-else>
-                  <span v-show="!isEdit2">{{ freight.ruleValue }}</span>
-                  <el-input-number v-show="isEdit2" v-model="freight.ruleValue" :controls="false" :precision="2" :min="0" :placeholder="`${freight.cnName}`" style="width:110px;" @blur="handlerBlur(scope.row)" />
-                </div>
-              </el-form-item>
+        <el-table-column width="160" label="装货数量" align="left" prop="loadWeight">
+          <template slot-scope="scope">
+            <span v-if="scope.row.isDregs === 1">{{ scope.row.loadWeight }}</span>
+            <div v-else>
+              <el-input-number v-if="scope.row.stowageStatus !== '2'" v-model="scope.row.loadWeight" :controls="false" placeholder="请输入装货数量" style="width:100%;" size="mini" @blur="handlerBlur(scope.row)" />
+              <span v-else>{{ scope.row.loadWeight }}</span>
             </div>
 
-          </el-form>
-        </template>
-      </el-table-column>
-      <!-- 扣费项目 -->
-      <el-table-column align="center" width="420">
-        <template slot="header">
-          <span>扣费项目 <el-button type="text" @click="isEdit = !isEdit"><i class="el-icon-edit" /></el-button></span>
-        </template>
-
-        <template slot-scope="scope">
-          <span v-if="scope.row.isDregs == 1"> -- </span>
-
-          <el-form v-else :inline="true" label-position="right" size="mini" class="ly-flex" label-width="110px">
-            <div v-for="(freight, index) in scope.row.deductionFreightList" :key="index">
-              <el-form-item :label="freight.cnName + '(元)'">
-                <div>
-                  <span v-show="!isEdit">{{ freight.ruleValue }}</span>
-                  <el-input-number v-show="isEdit" v-model="freight.ruleValue" :controls="false" :precision="2" :min="0" :placeholder="`${freight.cnName}`" style="width:110px;" @blur="handlerBlur(scope.row)" />
-                </div>
-              </el-form-item>
+          </template>
+        </el-table-column>
+        <el-table-column width="160" label="卸货数量" align="left" prop="unloadWeight">
+          <template slot-scope="scope">
+            <span v-if="scope.row.isDregs === 1">{{ scope.row.unloadWeight }}</span>
+            <div v-else>
+              <el-input-number v-if="scope.row.stowageStatus !== '2'" v-model="scope.row.unloadWeight" :controls="false" placeholder="请输入卸货数量" style="width:100%;" size="mini" @blur="handlerBlur(scope.row)" />
+              <span v-else>{{ scope.row.unloadWeight }}</span>
             </div>
-          </el-form>
-        </template>
-      </el-table-column>
+          </template>
+        </el-table-column>
+
+        <el-table-column width="160" label="配载方式" align="center" prop="lossAllowScope">
+          <template slot-scope="scope">
+            <span v-show="scope.row.stowageStatus === '0'">
+              吨数配载
+            </span>
+            <span v-show="scope.row.stowageStatus === '1'">
+              方数配载
+            </span>
+            <span v-show="scope.row.stowageStatus === '2'">
+              车数配载
+            </span>
+          </template>
+        </el-table-column>
+
+        <el-table-column width="160" label="路耗(吨/方)" align="center" prop="loss">
+          <template slot-scope="scope">
+            <span v-if="scope.row.stowageStatus === '0'">{{ floor((scope.row.loss -0), 2) }}</span>
+            <span v-else>{{ scope.row.loss || 0 }}</span>
+          </template>
+        </el-table-column>
+
+        <el-table-column width="160" label="路耗允许范围(吨/方)" align="center" prop="lossAllowScope">
+          <template slot-scope="scope">
+            <span>{{ scope.row.lossAllowScope? _lossAllowScope(scope.row.lossAllowScope, scope.row.stowageStatus === '0' ) : '--' }}</span>
+          </template>
+        </el-table-column>
+
+        <el-table-column width="160" label="货物单价(元)" align="center" prop="goodsPrice" />
+
+        <el-table-column width="160" label="货主成交单价(元)" align="center" prop="freightPrice" />
+
+        <el-table-column width="160" label="司机成交单价(元)" align="center" prop="freightPriceDriver" />
+        <el-table-column width="160" label="亏涨扣费(元)" align="center" prop="lossDeductionFee" />
+
+
+        <el-table-column width="120" label="抹零金额(元)" align="center" prop="m0Fee" />
+
+        <el-table-column width="160" label="司机实收运费(元)" align="center" prop="deliveryFeePractical" />
+
+        <!-- 补贴项目 -->
+        <el-table-column align="center" width="420" label="补贴项目">
+          <template slot="header">
+            <span>补贴项目
+              <el-button type="text" @click="isEdit2 = !isEdit2"><i class="el-icon-edit" /></el-button>
+
+              <!-- <el-button type="text" @click="handlerPlus"><i class="el-icon-plus" /></el-button> -->
+              <!-- 1: 是增 2: 是减 -->
+              <PopoverCom :list="showSubList" />
+            </span>
+
+          </template>
+          <template slot-scope="scope">
+            <span v-if="scope.row.isDregs == 1"> -- </span>
+
+            <el-form v-else :inline="true" label-position="right" size="mini" class="ly-flex" label-width="110px">
+              <div v-for="(freight, index) in scope.row.subsidiesFreightList" :key="index">
+                <!-- 渣土 其他不能修改 -->
+                <el-form-item :label="freight.cnName + '(元)'">
+                  <span v-if="scope.row.isDregs === 1">{{ freight.ruleValue }}</span>
+                  <div v-else>
+                    <span v-show="!isEdit2">{{ freight.ruleValue }}</span>
+                    <el-input-number v-show="isEdit2" v-model="freight.ruleValue" :controls="false" :precision="2" :min="0" :placeholder="`${freight.cnName}`" style="width:110px;" @blur="handlerBlur(scope.row)" />
+                  </div>
+                </el-form-item>
+              </div>
+
+            </el-form>
+          </template>
+        </el-table-column>
+        <!-- 扣费项目 -->
+        <el-table-column align="center" width="420">
+          <template slot="header">
+            <span>扣费项目 <el-button type="text" @click="isEdit = !isEdit"><i class="el-icon-edit" /></el-button></span>
+          </template>
+
+          <template slot-scope="scope">
+            <span v-if="scope.row.isDregs == 1"> -- </span>
+
+            <el-form v-else :inline="true" label-position="right" size="mini" class="ly-flex" label-width="110px">
+              <div v-for="(freight, index) in scope.row.deductionFreightList" :key="index">
+                <el-form-item :label="freight.cnName + '(元)'">
+                  <div>
+                    <span v-show="!isEdit">{{ freight.ruleValue }}</span>
+                    <el-input-number v-show="isEdit" v-model="freight.ruleValue" :controls="false" :precision="2" :min="0" :placeholder="`${freight.cnName}`" style="width:110px;" @blur="handlerBlur(scope.row)" />
+                  </div>
+                </el-form-item>
+              </div>
+            </el-form>
+          </template>
+        </el-table-column>
 
 
 
-      <el-table-column width="120" label="纳税金额(元)" align="center" prop="taxPayment" fixed="right">
-        <template slot-scope="scope">
-          <span> {{ floor(scope.row.taxPayment) }} </span>
-        </template>
-      </el-table-column>
-      <el-table-column width="120" label="服务费(元)" align="center" prop="serviceFee" fixed="right">
-        <template slot-scope="scope">
-          <span> {{ floor(scope.row.serviceFee) }} </span>
-        </template>
-      </el-table-column>
-      <el-table-column width="162" label="司机实收金额(元)" align="center" prop="deliveryCashFee" fixed="right">
-        <template slot-scope="scope">
-          <span>{{ floor(scope.row.deliveryCashFee) }}</span>
-        </template>
-      </el-table-column>
+        <el-table-column width="120" label="纳税金额(元)" align="center" prop="taxPayment" fixed="right">
+          <template slot-scope="scope">
+            <span> {{ floor(scope.row.taxPayment) }} </span>
+          </template>
+        </el-table-column>
+        <el-table-column width="120" label="服务费(元)" align="center" prop="serviceFee" fixed="right">
+          <template slot-scope="scope">
+            <span> {{ floor(scope.row.serviceFee) }} </span>
+          </template>
+        </el-table-column>
+        <el-table-column width="162" label="司机实收金额(元)" align="center" prop="deliveryCashFee" fixed="right">
+          <template slot-scope="scope">
+            <span>{{ floor(scope.row.deliveryCashFee) }}</span>
+          </template>
+        </el-table-column>
 
-      <el-table-column width="140" label="货主实付金额(元)" align="center" prop="shipperRealPay" fixed="right">
-        <template slot-scope="scope">
-          <span> {{ floor(scope.row.shipperRealPay) }} </span>
-        </template>
-      </el-table-column>
+        <el-table-column width="140" label="货主实付金额(元)" align="center" prop="shipperRealPay" fixed="right">
+          <template slot-scope="scope">
+            <span> {{ floor(scope.row.shipperRealPay) }} </span>
+          </template>
+        </el-table-column>
 
-    </el-table>
+      </el-table>
 
-    <div slot="footer" class="dialog-footer">
-      <el-button type="primary" @click="submitForm">立即核算</el-button>
-      <el-button @click="cancel">返回</el-button>
-    </div>
-  </el-dialog>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">立即核算</el-button>
+        <el-button @click="cancel">返回</el-button>
+      </div>
+    </el-dialog>
+
+
+  </div>
 </template>
 
 <script>
+
 import PopoverCom from './components/PopoverCom';
+// import chooseItemDialog from '@/views/enterprise/rules/chooseItemDialog';
 import { adjustDetail, calculateFee, batchCheck } from '@/api/settlement/adjust';
+import { getRuleItemList } from '@/api/enterprise/rules';
 import { floor } from '@/utils/ddc';
+
 
 export default {
   name: 'AdjustDialog',
@@ -168,6 +180,14 @@ export default {
   },
   data() {
     return {
+
+      showSubList: [],
+      // fixed: [], // 固定的规则
+      // title12: '',
+      // open12: false,
+      // shipmentCode: '',
+
+      //
       isPiliang: false,
       isEdit2: false,
       isEdit: false,
@@ -193,9 +213,26 @@ export default {
       }
     }
   },
+
+  // watch: {
+  //   ruleDetails(val) {
+  //     console.log(val);
+  //   }
+  // },
   created() {
+    this.getRuleLists();
   },
+
   methods: {
+    handlerInput(val) {
+      console.log(val);
+      this.adjustlist.forEach(e => {
+        e.subsidiesFreightList = val;
+      });
+      // this.$forceUpdate();
+    },
+
+
     // 单项修改
     async handlerBlur(row) {
       const {
@@ -314,10 +351,37 @@ export default {
       adjustDetail(this.queryParams).then(response => {
         this.adjustlist = JSON.parse(JSON.stringify(response.data));
 
+        const felexes = [...this.adjustlist[0].subsidiesFreightList, ...this.adjustlist[0].deductionFreightList];
+
+
+
+        this.showSubList.forEach(e => {
+          felexes.forEach(ee => {
+            if (e.enName === ee.enName) {
+              e.ee = ee;
+              e.$_disabled = true;
+            }
+          });
+        });
+        console.log(this.showSubList);
+
         this.total = response.total;
         this.loading = false;
       });
     },
+
+    getRuleLists() {
+      const que = {
+        ruleType: 0
+      };
+      getRuleItemList(que).then(response => {
+        this.showSubList = response.data.list.filter(e => {
+          return e.showType === 1;
+        });
+      });
+    },
+
+
     /** 取消按钮 */
     cancel() {
       this.close();
@@ -336,8 +400,11 @@ export default {
     },
 
     // 处理增减
-    hadlerPlus() {
-
+    handlerPlus() {
+      this.$refs.RulesDialog.reset();
+      this.open12 = true;
+      this.title12 = '添加';
+      this.$refs.RulesDialog.setLossList();
     },
 
     /* 计算价格 */
--- src/views/settlement/adjust/components/PopoverCom.vue
+++ src/views/settlement/adjust/components/PopoverCom.vue
@@ -4,25 +4,234 @@
     width="400"
     trigger="click"
   >
-    <el-table :data="gridData">
-      <el-table-column width="150" property="date" label="日期" />
-      <el-table-column width="100" property="name" label="姓名" />
-      <el-table-column width="300" property="address" label="地址" />
-    </el-table>
-    <el-button slot="reference" type="text"><i class="el-icon-plus" /></el-button>
+    <div>
+      <el-checkbox-group v-model="checkboxGroup" @change="handlerChange">
+
+        <el-row>
+          <el-col v-for="item in showList" :key="item.enName" :span="8" class="mt10">
+            <el-checkbox style="width:100px;" :label="item.cnName" border :disabled="item.$_disabled" />
+          </el-col>
+        </el-row>
+      </el-checkbox-group>
+    </div>
+    <el-button slot="reference" type="text" @click="handlergetLists"><i class="el-icon-plus" /></el-button>
   </el-popover>
 </template>
 
 <script>
+// import { getRuleItemList } from '@/api/enterprise/rules';
 export default {
   name: 'PopoverCom',
   props: {
+    // value: {
+    //   type: Array,
+    //   default: () => []
+    // },
+    // type: {
+    //   type: [String, Number],
+    //   default: 1
+    // },
+    // fixed: {
+    //   type: Array,
+    //   default: () => []
+    // },
     list: {
       type: Array,
-      default: () => []
+      default: () => [] ||
+        [
+          {
+            'id': null,
+            'cnName': '其他',
+            'enName': 'OTHER',
+            'showType': 1,
+            'dictCode': null,
+            'isDel': 0,
+            'createCode': '2',
+            'createTime': '2021-04-07T06:36:07.000+00:00',
+            'updateCode': null,
+            'updateTime': null,
+            'code': '6',
+            'ruleType': 0
+          },
+          {
+            'id': null,
+            'cnName': '抹零规则',
+            'enName': 'M0',
+            'showType': 3,
+            'dictCode': 'M0',
+            'isDel': 0,
+            'createCode': '2',
+            'createTime': '2021-03-25T11:25:23.000+00:00',
+            'updateCode': null,
+            'updateTime': null,
+            'code': '18',
+            'ruleType': 0
+          },
+          {
+            'id': null,
+            'cnName': '油费',
+            'enName': 'FUEL',
+            'showType': 1,
+            'dictCode': null,
+            'isDel': 0,
+            'createCode': '2',
+            'createTime': '2021-03-19T06:33:57.000+00:00',
+            'updateCode': null,
+            'updateTime': null,
+            'code': '13',
+            'ruleType': 0
+          },
+          {
+            'id': null,
+            'cnName': 'ETC费',
+            'enName': 'ETC',
+            'showType': 1,
+            'dictCode': null,
+            'isDel': 0,
+            'createCode': '2',
+            'createTime': '2021-03-19T06:33:57.000+00:00',
+            'updateCode': null,
+            'updateTime': null,
+            'code': '14',
+            'ruleType': 0
+          },
+          {
+            'id': null,
+            'cnName': '扣回单费',
+            'enName': 'RECEIPT_FEE',
+            'showType': 1,
+            'dictCode': null,
+            'isDel': 0,
+            'createCode': '2',
+            'createTime': '2021-03-19T06:33:57.000+00:00',
+            'updateCode': null,
+            'updateTime': null,
+            'code': '16',
+            'ruleType': 0
+          },
+          {
+            'id': null,
+            'cnName': '超时费',
+            'enName': 'OVERTIME_FEE',
+            'showType': 1,
+            'dictCode': null,
+            'isDel': 0,
+            'createCode': '2',
+            'createTime': '2021-03-19T06:33:57.000+00:00',
+            'updateCode': null,
+            'updateTime': null,
+            'code': '15',
+            'ruleType': 0
+          },
+          {
+            'id': null,
+            'cnName': '卸车费',
+            'enName': 'DISCHARGE_FARE',
+            'showType': 1,
+            'dictCode': null,
+            'isDel': 0,
+            'createCode': '2',
+            'createTime': '2021-03-19T06:33:57.000+00:00',
+            'updateCode': null,
+            'updateTime': null,
+            'code': '12',
+            'ruleType': 0
+          },
+          {
+            'id': null,
+            'cnName': '装车费',
+            'enName': 'LOADING_FEE',
+            'showType': 1,
+            'dictCode': null,
+            'isDel': 0,
+            'createCode': '2',
+            'createTime': '2021-03-19T06:32:59.000+00:00',
+            'updateCode': null,
+            'updateTime': null,
+            'code': '11',
+            'ruleType': 0
+          }
+        ]
     }
-  }
+  },
 
+  data() {
+    return {
+      showList: [],
+      checkboxGroup: []
+    };
+  },
+
+
+  // watch: {
+  //   value: {
+  //     handler(data) {
+  //       console.log(data);
+  //       if (data) {
+  //         this.checkboxGroup.map(e => e.cnName);
+  //       }
+  //     },
+  //     immediate: true
+  //   },
+
+  //   fixed: {
+  //     handler(fixed) {
+  //       if (fixed.length) {
+  //         const arr = this.list.filter(e => {
+  //           let bool = true;
+  //           fixed.forEach(ee => {
+  //             if (ee.cnName === e.cnName) {
+  //               bool = false;
+  //             }
+  //           });
+
+  //           return bool;
+  //         });
+  //         console.log(arr, '选项1231');
+  //       }
+  //     },
+  //     immediate: true
+  //   }
+
+
+  // },
+
+  created() {},
+  methods: {
+    handlergetLists() {
+      this.showList = this.list;
+    },
+    handlerChange() {
+      const arr = [];
+      this.showList.forEach(e => {
+        this.checkboxGroup.forEach(ee => {
+          if (ee === e.cnName) {
+            arr.push({
+              ...e,
+              ruleValue: e.ruleValue || 0,
+              // type: this.type,
+              waybillCode: undefined
+            });
+          }
+        });
+      });
+
+      console.log(arr);
+
+      this.$emit('input', arr);
+    }
+
+    // getList() {
+    //   const que = {
+    //     ruleType: 0
+    //   };
+    //   getRuleItemList(que).then(response => {
+    //     this.list = response.data.list.filter(e => {
+    //       return e.showType === 1;
+    //     });
+    //   });
+    // }
+  }
 };
 </script>
 
