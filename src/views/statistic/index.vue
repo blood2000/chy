77791a62500ef216d1fc6bcfcfdeb933b199aa7e
LinCH <312329768>
2021-06-28 15:45:29 +0800

    - fix : 修改ws地址配置

--- src/views/statistic/index.vue
+++ src/views/statistic/index.vue
@@ -180,7 +180,7 @@ export default {
     // 创建websocket
     createWebSocket() {
       try {
-        this.websock = new WebSocket('ws://' + process.env.VUE_APP_BASE_WS + this.wsurl + '?branchCode=' + this.branchCode);
+        this.websock = new WebSocket('ws://' + process.env.VUE_APP_BASE_HOST + this.wsurl + '?branchCode=' + this.branchCode);
         this.initWebSocket();
       } catch (e) {
         console.log('catch', e);
