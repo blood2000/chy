92ae5407ee2a2faed037817747a91ac157f5b2cc
chenhj <121459108@qq.com>
2021-07-03 16:35:18 +0800

    隐藏注册为司机和注册为调度者按钮

--- src/views/assets/register/index.vue
+++ src/views/assets/register/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-      <el-alert
-              title="未分配角色的用户，分配为司机或者调度者成功后，请转至运力>司机管理/调度者管理下审核！"
-              type="warning">
-      </el-alert>
+    <el-alert
+      title="未分配角色的用户，分配为司机或者调度者成功后，请转至运力>司机管理/调度者管理下审核！"
+      type="warning"
+    />
     <div v-show="showSearch" class="app-container app-container--search">
       <el-form ref="queryForm" :model="queryParams" :inline="true" label-width="100px">
         <el-form-item label="手机号" prop="phonenumber">
@@ -35,51 +35,53 @@
       </el-form>
     </div>
     <div class="app-container">
-        <el-table v-loading="loading" highlight-current-row border :data="list">
-            <el-table-column
-                    label="操作"
-                    align="center"
-                    fixed="left"
-                    width="300"
-                    class-name="small-padding fixed-width"
-            >
-                <template slot-scope="scope">
-                    <el-button
-                            size="mini"
-                            type="text"
-                            @click="handleDriver(scope.row)"
-                    >注册为司机</el-button>
-                    <el-button
-                            size="mini"
-                            type="text"
-                            @click="handleTeam(scope.row)"
-                    >注册为调度者</el-button>
-                    <el-button
-                            size="mini"
-                            type="text"
-                            @click="handleDelete(scope.row)"
-                    >删除</el-button>
-                </template>
-            </el-table-column>
-            <el-table-column label="手机号" align="center" prop="phonenumber" :show-overflow-tooltip="true" />
-            <el-table-column label="用户姓名" align="center" prop="nickName" :show-overflow-tooltip="true" />
-            <el-table-column label="创建时间" align="center" prop="createTime" width="160">
-                <template slot-scope="scope">
-                    <span>{{ parseTime(scope.row.createTime) }}</span>
-                </template>
-            </el-table-column>
-        </el-table>
+      <el-table v-loading="loading" highlight-current-row border :data="list">
+        <el-table-column
+          label="操作"
+          align="center"
+          fixed="left"
+          width="300"
+          class-name="small-padding fixed-width"
+        >
+          <template slot-scope="scope">
+            <el-button
+              v-if="false"
+              size="mini"
+              type="text"
+              @click="handleDriver(scope.row)"
+            >注册为司机</el-button>
+            <el-button
+              v-if="false"
+              size="mini"
+              type="text"
+              @click="handleTeam(scope.row)"
+            >注册为调度者</el-button>
+            <el-button
+              size="mini"
+              type="text"
+              @click="handleDelete(scope.row)"
+            >删除</el-button>
+          </template>
+        </el-table-column>
+        <el-table-column label="手机号" align="center" prop="phonenumber" :show-overflow-tooltip="true" />
+        <el-table-column label="用户姓名" align="center" prop="nickName" :show-overflow-tooltip="true" />
+        <el-table-column label="创建时间" align="center" prop="createTime" width="160">
+          <template slot-scope="scope">
+            <span>{{ parseTime(scope.row.createTime) }}</span>
+          </template>
+        </el-table-column>
+      </el-table>
 
-        <pagination
-                v-show="total>0"
-                :total="total"
-                :page.sync="queryParams.pageNum"
-                :limit.sync="queryParams.pageSize"
-                @pagination="getList"
-        />
+      <pagination
+        v-show="total>0"
+        :total="total"
+        :page.sync="queryParams.pageNum"
+        :limit.sync="queryParams.pageSize"
+        @pagination="getList"
+      />
     </div>
-      <RegisterDriver :data="rowData" :open.sync="driverOpen"  @refresh="getList" ></RegisterDriver>
-      <RegisterTeam :data="rowData" :open.sync="teamOpen"  @refresh="getList" ></RegisterTeam>
+    <RegisterDriver :data="rowData" :open.sync="driverOpen" @refresh="getList" />
+    <RegisterTeam :data="rowData" :open.sync="teamOpen" @refresh="getList" />
   </div>
 </template>
 
@@ -89,7 +91,7 @@ import RegisterDriver from './driverDialog';
 import RegisterTeam from './teamDialog';
 export default {
   name: 'RegisterUser',
-  components: { RegisterDriver , RegisterTeam},
+  components: { RegisterDriver, RegisterTeam },
   data() {
     return {
       // 遮罩层
