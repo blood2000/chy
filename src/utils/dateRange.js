7372dc07b2bc0c17473b88746fd4d5f56b2971fa
fanrk <442189385@qq.com>
2021-07-09 16:21:10 +0800

    时间筛选与批量打款

--- src/utils/dateRange.js
+++ src/utils/dateRange.js
@@ -7,6 +7,23 @@ export const pickerOptions = {
       picker.$emit('pick', [start, end]);
     }
   }, {
+    text: '昨日',
+    onClick(picker) {
+      const end = new Date();
+      const start = new Date();
+      end.setTime(end.getTime() - 3600 * 1000 * 24 * 1);
+      start.setTime(start.getTime() - 3600 * 1000 * 24 * 1);
+      picker.$emit('pick', [start, end]);
+    }
+  }, {
+    text: '本周',
+    onClick(picker) {
+      const end = new Date();
+      const start = new Date();
+      start.setTime(start.getTime() - (start.getDay() - 1) * 3600 * 1000 * 24);
+      picker.$emit('pick', [start, end]);
+    }
+  }, {
     text: '本月',
     onClick(picker) {
       const end = new Date();
@@ -17,12 +34,36 @@ export const pickerOptions = {
       picker.$emit('pick', [start, end]);
     }
   }, {
-    text: '今年至今',
+    text: '本年',
     onClick(picker) {
       const end = new Date();
       const start = new Date();
       var tYear = end.getFullYear();
       start.setTime(new Date(tYear + '-01-01'));
+      picker.$emit('pick', [start, end]);
+    }
+  }, {
+    text: '近三天',
+    onClick(picker) {
+      const end = new Date();
+      const start = new Date();
+      start.setTime(start.getTime() - 3600 * 1000 * 24 * 2);
+      picker.$emit('pick', [start, end]);
+    }
+  }, {
+    text: '近七天',
+    onClick(picker) {
+      const end = new Date();
+      const start = new Date();
+      start.setTime(start.getTime() - 3600 * 1000 * 24 * 6);
+      picker.$emit('pick', [start, end]);
+    }
+  }, {
+    text: '近三月',
+    onClick(picker) {
+      const end = new Date();
+      const start = new Date();
+      start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
       picker.$emit('pick', [start, end]);
     }
   }]
