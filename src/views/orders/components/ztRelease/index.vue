79a839a8177fcede79af8751a151eaff311bfc0c
chenhj <121459108@qq.com>
2021-07-12 10:59:06 +0800

    渣土货源发布

--- src/views/orders/components/ztRelease/index.vue
+++ src/views/orders/components/ztRelease/index.vue
@@ -1,6 +1,7 @@
 <template>
   <el-form
     ref="elForm"
+    v-loading="loading"
     :model="formData"
     :rules="rules"
     size="medium"
@@ -11,29 +12,17 @@
       <div class="header mb8">渣土货源</div>
 
 
-      <el-form-item label="项目工程" prop="xiangmianigg">
+      <el-form-item label="项目工程" prop="projectCode">
         <!-- 待处理 -->
-        <el-tag v-if="formData.xiangmianigg" :type="'warning'">
-          {{ formData.xiangmianigg }}
+        <el-tag v-if="formData.projectCode" :type="'warning'">
+          {{ formData.projectName }}
         </el-tag>
         <el-button class="ml10" size="mini" type="primary" @click="openObj = true">请选择项目工程</el-button>
       </el-form-item>
 
-      <el-form-item label="选择调度" prop="diiaidajiogajo">
-        <!-- <el-select
-          v-model="formData.diiaidajiogajo"
-          placeholder="请选择调度"
-          filterable
-        >
-          <el-option
-            v-for="(dict,index) in diiaidajiogajo_option"
-            :key="index"
-            :label="dict.dictLabel"
-            :value="dict.dictValue"
-          />
-        </el-select> -->
+      <el-form-item label="选择调度" prop="dispatcherCodes">
 
-        <span>{{ formData.diiaidajiogajo }}</span>
+        <!-- <span>{{ formData.dispatcherCodes }}</span> -->
         <el-tag
           v-for="tag in schedSelect"
           :key="tag.code"
@@ -48,16 +37,21 @@
         <el-button class="ml10" size="mini" type="primary" @click="scheduling = true">请选择调度</el-button>
       </el-form-item>
 
-      <el-form-item label="渣土类型" prop="zhatuleix">
-        <el-radio-group v-model="formData.zhatuleix">
-          <el-radio :label="1">渣土场</el-radio>
-          <el-radio :label="2">场内</el-radio>
-          <el-radio :label="3">自倒</el-radio>
+      <el-form-item label="渣土类型" prop="orderType">
+        <el-radio-group v-model="formData.orderType" disabled>
+          <el-radio label="1200">渣土场</el-radio>
+          <el-radio label="2">场内</el-radio>
+          <el-radio label="3">自倒</el-radio>
         </el-radio-group>
       </el-form-item>
 
-      <el-form-item label="是否启用电子围栏" prop="isqiqihigii">
-        <el-switch v-model="formData.isqiqihigii" />
+      <el-form-item label="是否启用电子围栏" prop="geofenceToggle">
+        <el-switch v-model="formData.geofenceToggle" />
+      </el-form-item>
+
+      <el-form-item v-if="formData.geofenceToggle" label="围栏半径" prop="geofenceRadius">
+        <el-input-number v-model="formData.geofenceRadius" :min="0" label="请输入围栏半径" />
+        <el-button class="ml10" size="mini" type="primary" @click="hangdlerYulang">预览</el-button>
       </el-form-item>
 
       <el-form-item label="备注" prop="remark">
@@ -74,7 +68,7 @@
 
       <el-form-item>
         <el-button type="primary" plain>取 消</el-button>
-        <el-button type="primary">立即发布</el-button>
+        <el-button type="primary" :loading="loading" @click="submitForm('elForm')">立即发布</el-button>
       </el-form-item>
 
     </div>
@@ -88,7 +82,7 @@
         <el-form-item>
           <div class="ly-t-right">
             <el-button type="primary" plain @click="openObj = false">取 消</el-button>
-            <el-button type="primary" @click="formData.xiangmianigg = selectData.projectName; openObj = false;">确 定</el-button>
+            <el-button type="primary" @click="handlerProject">确 定</el-button>
           </div>
         </el-form-item>
       </div>
@@ -116,22 +110,35 @@ import ProjectIndex from '@/views/enterprise/project';
 import GroupIndex from '@/views/enterprise/group';
 
 import { getUserInfo } from '@/utils/auth';
+
+import { ztPublishOrder } from '@/api/order/release'; // 发布渣土请求
 export default {
   components: { ProjectIndex, GroupIndex },
 
   data() {
     return {
+      loading: false,
+
       'formData': {
-        'xiangmianigg': undefined,
-        'diiaidajiogajo': undefined,
-        'zhatuleix': 1,
-        'isqiqihigii': true,
-        'remark': ''
+        'projectName': undefined, // 项目名
+        'projectCode': undefined, // 项目Code		false
+        'dispatcherCodes': [], // 调度车队Code		false
+        'orderType': '1200', // 货源类型		false 默认写死
+        'geofenceToggle': false, // 电子围栏是否开启 1 是 0 否		false
+        'remark': '', // 备注		false
+        'geofenceRadius': 0, // 电子围栏范围		false
+        publishMode: 0, // 发布方式 0 货源大厅不可见(只能通过货单号或备注搜索) 1 货源大厅可见 默认写死
+
+        // 选填
+        'branchCode': undefined, //
+        'orgCode': undefined,
+        'publishCode': undefined //	发布人Code		false
       },
       'rules': {
-        xiangmianigg: [{ required: true, message: '请选择项目工程', trigger: 'change' }],
-        diiaidajiogajo: [{ required: true, message: '请选择调度', trigger: 'change' }],
-        zhatuleix: [{ required: true, trigger: 'change' }]
+        projectCode: [{ required: true, message: '请选择项目工程', trigger: 'change' }],
+        dispatcherCodes: [{ required: true, message: '请选择调度', trigger: 'change' }],
+        orderType: [{ required: true, trigger: 'change' }],
+        geofenceRadius: [{ required: true, message: '请输入围栏半径', trigger: 'blur' }]
       },
 
       'openObj': false, // 项目工程 弹框
@@ -141,8 +148,8 @@ export default {
       'schedSelect': [], // 选中的调度
 
 
-      xiangmianigg_option: [],
-      diiaidajiogajo_option: []
+      projectCode_option: [],
+      dispatcherCodes_option: []
     };
   },
 
@@ -164,14 +171,70 @@ export default {
 
   methods: {
 
+    // 项目
+    handlerProject() {
+      console.log(this.selectData, '项目'); // 选择好的项目
+      this.formData.projectCode = this.selectData.code;
+      this.formData.projectName = this.selectData.projectName;
+      this.openObj = false;
+    },
+
+    // 调度者
     handlerScheduling() {
-      // console.log(this.schedSelect);
+      console.log(this.schedSelect, '调度者');
+      this.formData.dispatcherCodes = this.schedSelect.map(e => e.disUserCode);
       this.scheduling = false;
     },
 
-    // 关闭 (tag)=>schedSelect = schedSelect.filter(e=> e.code !== tag.code)
+    // 关闭
     hamdlerClose(tag) {
       this.schedSelect = this.schedSelect.filter(e => e.code !== tag.code);
+    },
+
+    // 预览
+    hangdlerYulang() {
+      console.log(this.selectData);
+
+      if (JSON.stringify(this.selectData) === '{}' || !this.selectData.latitude || !this.selectData.longitude) {
+        this.$alert('请选择项目工程后才可进行设置围栏半径', '设置提示', {
+          confirmButtonText: '确定'
+        }).then(() => { this.openObj = true; }).catch(() => {});
+      } else {
+        const { latitude, longitude } = this.selectData;
+        console.log({ latitude, longitude });
+      }
+    },
+
+    // 提交
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          console.log(this.formData);
+
+          const qer = {
+            ...this.formData,
+            'geofenceToggle': this.formData.geofenceToggle ? 1 : 0, // 电子围栏是否开启 1 是 0 否		false
+            projectName: undefined // 不需要传这个
+          };
+
+          this.loading = true;
+          ztPublishOrder(qer).then(res => {
+            this.msgSuccess(res.msg);
+            // 跳转
+            var time1 = setTimeout(() => {
+              clearTimeout(time1);
+              time1 = null;
+              this.loading = false;
+              this.$router.push({ name: 'Manage', query: { p: Date.now() }});
+            }, 700);
+          }).catch(() => {
+            this.loading = false;
+          });
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
     }
   }
 
