a0b8793a410a6b94431d9026c10c3dbe54bb51a7
chenhj <121459108@qq.com>
2021-07-15 20:11:46 +0800

    优化

--- src/api/construction/manhour.js
+++ src/api/construction/manhour.js
@@ -1,10 +1,10 @@
 import request from '@/utils/request';
 
 // web机械工时列表
-export const getMachineWorkingapi = '/tools/machineWork/web—getMachineWorkingList';
+export const getMachineWorkingapi = '/project-kydsz/machineWork/web—getMachineWorkingList';
 export function getMachineWorkingList(data) {
   return request({
-    url: '/tools/machineWork/web—getMachineWorkingList',
+    url: '/project-kydsz/machineWork/web—getMachineWorkingList',
     method: 'post',
     data: data
   });
