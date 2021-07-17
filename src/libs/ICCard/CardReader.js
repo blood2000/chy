ca33249077d5cbc978b507008dc9c6d674109227
chenhj <121459108@qq.com>
2021-07-17 14:49:08 +0800

    bug修改

--- src/libs/ICCard/CardReader.js
+++ src/libs/ICCard/CardReader.js
@@ -102,7 +102,7 @@ const CardReader = {
         CardReader.socket.onmessage = function(e) {
           const ret = CardReader.fn.getResult(e.data);
           if (ret.success) {
-            console.log('读卡成功信息' + CardReader._cmdIndex + '：' + (CardReader.codes[ret.code] ? CardReader.codes[ret.code].message : ret.code));
+            console.log('读卡成功信息' + CardReader._cmdIndex + '：' + (CardReader.codes[ret.code] ? CardReader.codes[ret.code].message + ret.code : ret.code));
             resolve(e.data);
           } else {
             // console.log('错误信息', ret);
