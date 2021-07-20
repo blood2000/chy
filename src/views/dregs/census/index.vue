5cdeaaff33b076607e7e9d13a879df92488dbe85
huangst <313949553@qq.com>
2021-07-20 18:07:10 +0800

    渣土统计

--- src/views/dregs/census/index.vue
+++ src/views/dregs/census/index.vue
@@ -3,7 +3,7 @@
     <el-row :gutter="15">
       <el-col :xl="6" :lg="7" :md="8" :xs="24">
         <div class="app-container census-page-left">
-          <ul class="project-list">
+          <ul class="project-list census-scroll-box">
             <li v-for="(item, index) in projectList" :key="item.projectCode" :class="{active: projectActive === index}" class="project-list-item" @click="handleClickProjectItem(item, index)">
               <h5>项目：{{ item.projectName }}</h5>
               <el-row>
@@ -38,90 +38,92 @@
           <el-date-picker v-model="queryDate" value-format="yyyy-MM-dd" class="census-date-picker" size="large" type="date" placeholder="选择日期" @change="changeDetail" />
           <Tabs :tablist="tablist" @getActiveName="getActiveTab" />
           <div class="app-container">
-            <!-- 1.项目统计 -->
-            <div v-if="activeTab === '项目统计'" class="tab-page width60 own-census-table">
-              <el-row class="total_bg">
-                <el-col :span="2">
-                  <img src="@/assets/images/icon/total.png" alt="">
-                </el-col>
-                <el-col :span="5">
-                  <p style="line-height: 30px">车队：<span class="num">{{ projectStatistic.teamCount }}</span></p>
-                </el-col>
-                <el-col :span="5">
-                  <p style="line-height: 30px">车辆：<span class="num">{{ projectStatistic.vehicleCount }}</span></p>
-                </el-col>
-                <el-col :span="5">
-                  <p style="line-height: 30px">车次：<span class="num">{{ projectStatistic.trainNumber }}</span></p>
-                </el-col>
-              </el-row>
-              <el-collapse v-model="vechicleCode" accordion class="census-collapse" @change="changeProjectCollapse">
-                <el-collapse-item v-for="(item, index) in projectStatisticList" :key="index" :name="item.vehicleCode">
-                  <template slot="title">
-                    <h5>
-                      {{ item.licenseNumber }}
-                      <span>车次：{{ item.trainNumber }}</span>
-                    </h5>
-                  </template>
-                  <el-table v-loading="vechicleLoading" :data="carDetailList">
-                    <el-table-column type="index" label="编号" width="100" />
-                    <el-table-column label="卸货地" align="left" prop="unloadAddress" />
-                    <el-table-column label="装货时间" align="left" prop="loadTime" />
-                    <el-table-column label="经手人" align="left" prop="noteTaker" />
-                  </el-table>
-                </el-collapse-item>
-              </el-collapse>
-            </div>
-            <!-- 2.进出明细 -->
-            <div v-if="activeTab === '进出明细'" class="tab-page">
-              <el-table v-loading="inOutLoading" highlight-current-row :data="inOutList">
-                <el-table-column type="index" label="编号" width="60" />
-                <el-table-column label="车牌号" align="left" prop="licenseNumber" />
-                <el-table-column label="进场时间" align="left" prop="inTime" />
-                <el-table-column label="出场时间" align="left" prop="outTime" />
-                <el-table-column label="驾驶员姓名" align="left" prop="driverName" />
-                <el-table-column label="土石方量(m)²" align="left" prop="loadWeight" />
-                <el-table-column label="超高超载" align="left" prop="overload">
-                  <template slot-scope="scope">
-                    <p v-if="scope.row.overload === 1" class="text">
-                      <span class="g-color-error g-pot" />
-                      是
-                    </p>
-                    <p v-else-if="scope.row.overload === 0" class="text">
-                      <span class="g-color-success g-pot" />
-                      否
-                    </p>
-                    <p v-else class="text">未装货</p>
-                  </template>
-                </el-table-column>
-                <el-table-column label="泥尾" align="left" prop="mudtail" />
-                <el-table-column label="记录人" align="left" prop="noteTaker" />
-              </el-table>
-            </div>
-            <!-- 3.泥尾统计 -->
-            <div v-if="activeTab === '泥尾统计'" class="tab-page width60 own-census-table">
-              <el-row class="total_bg">
-                <el-col :span="2">
-                  <img src="@/assets/images/icon/total.png" alt="">
-                </el-col>
-                <el-col v-for="(item, index) in muckardCounts" :key="index" :span="5">
-                  <p style="line-height: 30px">{{ item.mudtail + '：' }}<span class="num">{{ item.trainNumber }}</span></p>
-                </el-col>
-                <el-col :span="5">
-                  <p style="line-height: 30px">合计: <span class="num">{{ muckardTotal }}</span></p>
-                </el-col>
-              </el-row>
-              <el-collapse v-model="muckardCollapse" class="census-collapse">
-                <el-collapse-item v-for="(item, index) in muckardList" :key="index" :name="item.licenseNumber">
-                  <template slot="title">
-                    <h5>{{ item.licenseNumber }}</h5>
-                  </template>
-                  <el-table :data="item.muckardCounts">
-                    <el-table-column type="index" label="编号" width="100" />
-                    <el-table-column label="泥尾" align="left" prop="mudtail" />
-                    <el-table-column label="车次" align="left" prop="trainNumber" />
-                  </el-table>
-                </el-collapse-item>
-              </el-collapse>
+            <div class="tab-page-wrap census-scroll-box">
+              <!-- 1.项目统计 -->
+              <div v-if="activeTab === '项目统计'" class="tab-page width60 own-census-table">
+                <el-row class="total_bg">
+                  <el-col :span="2">
+                    <img src="@/assets/images/icon/total.png" alt="">
+                  </el-col>
+                  <el-col :span="5">
+                    <p style="line-height: 30px">车队：<span class="num">{{ projectStatistic.teamCount }}</span></p>
+                  </el-col>
+                  <el-col :span="5">
+                    <p style="line-height: 30px">车辆：<span class="num">{{ projectStatistic.vehicleCount }}</span></p>
+                  </el-col>
+                  <el-col :span="5">
+                    <p style="line-height: 30px">车次：<span class="num">{{ projectStatistic.trainNumber }}</span></p>
+                  </el-col>
+                </el-row>
+                <el-collapse v-model="vechicleCode" accordion class="census-collapse" @change="changeProjectCollapse">
+                  <el-collapse-item v-for="(item, index) in projectStatisticList" :key="index" :name="item.vehicleCode">
+                    <template slot="title">
+                      <h5>
+                        {{ item.licenseNumber }}
+                        <span>车次：{{ item.trainNumber }}</span>
+                      </h5>
+                    </template>
+                    <el-table v-loading="vechicleLoading" :data="carDetailList">
+                      <el-table-column type="index" label="编号" width="100" />
+                      <el-table-column label="卸货地" align="left" prop="unloadAddress" />
+                      <el-table-column label="装货时间" align="left" prop="loadTime" />
+                      <el-table-column label="经手人" align="left" prop="noteTaker" />
+                    </el-table>
+                  </el-collapse-item>
+                </el-collapse>
+              </div>
+              <!-- 2.进出明细 -->
+              <div v-if="activeTab === '进出明细'" class="tab-page">
+                <el-table v-loading="inOutLoading" highlight-current-row :data="inOutList">
+                  <el-table-column type="index" label="编号" width="60" />
+                  <el-table-column label="车牌号" align="left" prop="licenseNumber" />
+                  <el-table-column label="进场时间" align="left" prop="inTime" />
+                  <el-table-column label="出场时间" align="left" prop="outTime" />
+                  <el-table-column label="驾驶员姓名" align="left" prop="driverName" />
+                  <el-table-column label="土石方量(m)²" align="left" prop="loadWeight" min-width="90" />
+                  <el-table-column label="超高超载" align="left" prop="overload">
+                    <template slot-scope="scope">
+                      <p v-if="scope.row.overload === 1" class="text">
+                        <span class="g-color-error g-pot" />
+                        是
+                      </p>
+                      <p v-else-if="scope.row.overload === 0" class="text">
+                        <span class="g-color-success g-pot" />
+                        否
+                      </p>
+                      <p v-else class="text">未装货</p>
+                    </template>
+                  </el-table-column>
+                  <el-table-column label="泥尾" align="left" prop="mudtail" />
+                  <el-table-column label="记录人" align="left" prop="noteTaker" />
+                </el-table>
+              </div>
+              <!-- 3.泥尾统计 -->
+              <div v-if="activeTab === '泥尾统计'" class="tab-page width60 own-census-table">
+                <el-row class="total_bg">
+                  <el-col :span="2">
+                    <img src="@/assets/images/icon/total.png" alt="">
+                  </el-col>
+                  <el-col v-for="(item, index) in muckardCounts" :key="index" :span="5">
+                    <p style="line-height: 30px">{{ item.mudtail + '：' }}<span class="num">{{ item.trainNumber }}</span></p>
+                  </el-col>
+                  <el-col :span="5">
+                    <p style="line-height: 30px">合计: <span class="num">{{ muckardTotal }}</span></p>
+                  </el-col>
+                </el-row>
+                <el-collapse v-model="muckardCollapse" class="census-collapse">
+                  <el-collapse-item v-for="(item, index) in muckardList" :key="index" :name="item.licenseNumber">
+                    <template slot="title">
+                      <h5>{{ item.licenseNumber }}</h5>
+                    </template>
+                    <el-table :data="item.muckardCounts">
+                      <el-table-column type="index" label="编号" width="100" />
+                      <el-table-column label="泥尾" align="left" prop="mudtail" />
+                      <el-table-column label="车次" align="left" prop="trainNumber" />
+                    </el-table>
+                  </el-collapse-item>
+                </el-collapse>
+              </div>
             </div>
           </div>
         </div>
@@ -300,8 +302,12 @@ export default {
   // 项目列表
   .census-page-left{
     margin: 0 0 15px;
-    min-height: calc(100vh - 210px);
+    height: calc(100vh - 210px);
+    padding: 20px 0 20px 20px;
     .project-list{
+      height: 100%;
+      overflow-y: auto;
+      padding-right: 15px;
       .project-list-item{
         margin-bottom: 24px;
         cursor: pointer;
@@ -321,7 +327,7 @@ export default {
           color: rgba(144, 147, 152, 0.7);
         }
         &.active{
-          height: 154px;
+          min-height: 154px;
           background: #0672D9;
           box-shadow: 0px 3px 6px rgba(67, 110, 207, 0.4);
           border-radius: 6px;
@@ -370,7 +376,13 @@ export default {
     }
     >.app-container{
       margin: 0;
-      min-height: calc(100vh - 210px);
+      height: calc(100vh - 302px);
+      padding: 20px 0 20px 20px;
+      .tab-page-wrap{
+        height: 100%;
+        padding-right: 20px;
+        overflow-y: auto;
+      }
       .total_bg{
         background: #F8F9FA;
         border-radius: 4px;
@@ -382,8 +394,9 @@ export default {
         }
       }
       .width60{
-        width: 60%;
-        min-width: 760px;
+        width: 100%;
+        min-width: 544px;
+        max-width: 900px;
       }
       // 折叠面板
       .census-collapse{
@@ -459,5 +472,17 @@ export default {
   .el-table__body td{
     color: rgba(0, 0, 0, 0.7);
   }
+}
+// 滚动条样式
+.census-scroll-box::-webkit-scrollbar {
+  width: 6px;     /*高宽分别对应横竖滚动条的尺寸*/
+  height: 6px;
+}
+.census-scroll-box::-webkit-scrollbar-thumb {/*滚动条里面小方块*/
+  border-radius: 6px;
+  background: rgba(0, 0, 0, 0.1);
+}
+.census-scroll-box::-webkit-scrollbar-thumb:hover {
+  background:rgba(0, 0, 0, 0.2);
 }
 </style>
