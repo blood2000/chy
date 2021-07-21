608aae64414bc3894e5a483ed88581410f5e2726
tyuxiu <tyx19921018>
2021-07-21 17:16:45 +0800

    优化

--- vue.config.js
+++ vue.config.js
@@ -35,10 +35,10 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        // target: process.env.VUE_PROTOCOL+process.env.VUE_APP_BASE_HOST, // 测试
+        target: process.env.VUE_PROTOCOL+process.env.VUE_APP_BASE_HOST, // 测试
         // target: `http://192.168.30.1:8080`, // 绍
         // target: `http://192.168.30.171:8080`, // 觉
-        target: `http://192.168.30.239:8080`, //秀
+        // target: `http://192.168.30.239:8080`, //秀
         // target: `http://192.168.30.186:8080`, //同
         //target: `http://192.168.30.134:8080`, //志
         // target: `http://10.0.0.75:8080`,
