31e63ec6604d936450f0e7a914f1ee0ca96862b6
chenhj <121459108@qq.com>
2021-08-23 10:46:33 +0800

    机械工时列表的接口地址改变

--- src/api/construction/manhour.js
+++ src/api/construction/manhour.js
@@ -1,10 +1,10 @@
 import request from '@/utils/request';
 
 // web机械工时列表
-export const getMachineWorkingapi = '/kydsz/machineWork/web—getMachineWorkingList';
+export const getMachineWorkingapi = '/kydsz/machineWorkHours/web—getMachineWorkingList';
 export function getMachineWorkingList(data) {
   return request({
-    url: '/kydsz/machineWork/web—getMachineWorkingList',
+    url: '/kydsz/machineWorkHours/web—getMachineWorkingList',
     method: 'post',
     data: data
   });
