e87954d8399f8d8725be325dfa20029349504740
huangst <313949553@qq.com>
2021-12-30 18:07:35 +0800

    refactor：元素、模型的位置支持%单位

--- src/views/dataCube/mixins/utils.js
+++ src/views/dataCube/mixins/utils.js
@@ -22,18 +22,24 @@ export default {
   methods: {
     numberToPx(obj) {
       const style = Object.assign({}, obj);
-      const { isFitWidth, isFitHeight } = style;
+      const { isFitWidth, isFitHeight, isFitTop, isFitLeft } = style;
       for (const key in style) {
         if (key === 'width' && isFitWidth) {
           style[key] = style[key] + isFitWidth;
         } else if (key === 'height' && isFitHeight) {
           style[key] = style[key] + isFitHeight;
+        } else if (key === 'top' && isFitTop) {
+          style[key] = style[key] + isFitTop;
+        } else if (key === 'left' && isFitLeft) {
+          style[key] = style[key] + isFitLeft;
         } else if (this.numberToPxMap[key]) {
           style[key] = style[key] + 'px';
         }
       }
       if (isFitWidth) delete style[isFitWidth];
       if (isFitHeight) delete style[isFitHeight];
+      if (isFitTop) delete style[isFitTop];
+      if (isFitLeft) delete style[isFitLeft];
       return style;
     },
     removePosition(obj) {
