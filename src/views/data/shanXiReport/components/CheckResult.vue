227ef8490417a1e04a736a133f6198fc646cbffc
ruanzn <1725043241@qq.com>
2022-01-12 11:07:27 +0800

    （1）新增金融体系查询条件；（2）新增山西数据上报页面。

---
+++ src/views/data/shanXiReport/components/CheckResult.vue
@@ -0,0 +1,161 @@
+<template>
+  <div>
+
+    <el-row :gutter="10" class="mb8">
+
+      <!-- 做切换使用 -->
+      <el-radio-group v-model="active" size="small" @change="handleActive">
+        <el-radio-button v-for="(tab, index) in tabData" :key="index + tab.value" :class="!tab.isOk?'m_red': null" :label="tab.value">{{ tab.label }}</el-radio-button>
+      </el-radio-group>
+    </el-row>
+
+
+    <RefactorTable
+      is-show-index
+      :loading="loading"
+      :data="list"
+      :table-columns-config="tableColumnsConfig"
+    />
+
+    <div class="ly-t-center mt20 mb20">
+      <el-button type="primary" @click="_ok(true)">修 改</el-button>
+      <el-button type="primary" plain @click="_ok(false)">取 消</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    propData: {
+      type: Object,
+      default: null
+    }
+  },
+  data() {
+    return {
+      active: '0',
+      tabData: [
+        {
+          label: '驾驶员基本信息',
+          value: '0',
+          isOk: true,
+          list: [{ ttttttt1: 1 }]
+        },
+        {
+          label: '车辆基本信息',
+          value: '1',
+          isOk: true,
+          list: [{ ttttttt1: 333 }]
+        },
+        {
+          label: '运单信息',
+          value: '2',
+          isOk: true,
+          list: []
+        },
+        {
+          label: '装货位置',
+          value: '3',
+          isOk: true,
+          list: []
+        },
+        {
+          label: '卸货位置',
+          value: '4',
+          isOk: true,
+          list: []
+        },
+        {
+          label: '资金流水信息',
+          value: '5',
+          isOk: false,
+          list: []
+        }
+
+      ],
+
+      /* 表格 */
+      loading: false,
+      tableColumnsConfig: [
+        {
+          prop: 'ttttttt1',
+          isShow: true,
+          label: '监管平台字段'
+          // width: 100,
+          // fixed: 'left'
+        },
+        {
+          prop: 'ttttttt1',
+          isShow: true,
+          label: '超好运字段'
+          // width: 100,
+          // fixed: 'left'
+        },
+        {
+          prop: 'ttttttt1',
+          isShow: true,
+          label: '是否必填'
+          // width: 100,
+          // fixed: 'left'
+        },
+        {
+          prop: 'ttttttt1',
+          isShow: true,
+          label: '上传值'
+          // width: 100,
+          // fixed: 'left'
+        },
+        {
+          prop: 'ttttttt1',
+          isShow: true,
+          label: '本地校验'
+          // width: 100,
+          // fixed: 'left'
+        },
+        {
+          prop: 'ttttttt1',
+          isShow: true,
+          label: '错误内容'
+          // width: 100,
+          // fixed: 'left'
+        },
+        {
+          prop: 'ttttttt1',
+          isShow: true,
+          label: '标记'
+          // width: 100,
+          // fixed: 'left'
+        }
+      ]
+    };
+  },
+
+  computed: {
+    list() {
+      return this.tabData[this.active - 0].list;
+    }
+  },
+
+  created() {
+    // 1. 返回是个数组, 且每一个数组里面要一个判断是否合格
+
+    // 2. 列表 值?? 放在tabData里面
+    console.log(213, this.propData);
+  },
+
+  methods: {
+    handleActive() {},
+
+    _ok(bool) {
+      this.$emit('refresh', bool);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.m_red /deep/ .el-radio-button__inner{
+    color: red;
+}
+</style>
---
+++ src/views/data/shanXiReport/data-index.js
@@ -0,0 +1,57 @@
+export default [
+  {
+    prop: 'driverReport',
+    isShow: true,
+    label: '司机信息上报',
+    width: 100,
+    fixed: 'left'
+  },
+  {
+    prop: 'vehicleReport',
+    isShow: true,
+    label: '车辆信息上报',
+    width: 100,
+    fixed: 'left'
+  },
+  {
+    prop: 'waybillSendStatus',
+    isShow: true,
+    label: '运单上报',
+    width: 100,
+    fixed: 'left'
+  },
+  {
+    prop: 'billSendStatus',
+    isShow: true,
+    label: '流水上报',
+    width: 100,
+    fixed: 'left'
+  },
+  {
+    prop: 'waybillNo',
+    isShow: true,
+    label: '运输单号',
+    width: 160,
+    fixed: 'left'
+  },
+  {
+    prop: 'reportMarkStatus',
+    isShow: true,
+    label: '上报异常',
+    width: 75,
+    fixed: 'left'
+  },
+  {
+    prop: 'edit',
+    isShow: true,
+    label: '操作',
+    width: 180,
+    fixed: 'left'
+  }
+];
+
+// redisAddressList	装货卸货地址信息列表	array	货源商品装货卸货地址_3
+// redisOrderClassGoodsVoList	货集码列表	array	货源（分类）码编号_2
+// redisOrderFreightInfoVoList	运费规则商品下的地址下的规则	array	RedisOrderFreightInfoVo
+// redisOrderGoodsVoList		array	货源商品信息_2
+// redisOrderSpecifiedVoList		array	货源指定接单人表_1
