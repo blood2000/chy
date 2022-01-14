0552f2b9a505d6868989a85651f67817856a7aec
ruanzn <1725043241@qq.com>
2022-01-14 11:04:43 +0800

    接单时间：取消默认填写条件

--- src/views/waybill/manages/components/Waybill.vue
+++ src/views/waybill/manages/components/Waybill.vue
@@ -662,7 +662,7 @@ export default {
     });
     this.queryParams.startReceiveTime = this.parseTime(new Date().getTime() - 24 * 60 * 60 * 1000 * 2, '{y}-{m}-{d} 00:00:00');
     this.queryParams.endReceiveTime = this.parseTime(new Date(), '{y}-{m}-{d} 23:59:59');
-    this.receiveTime = [new Date(this.parseTime(new Date().getTime() - 24 * 60 * 60 * 1000 * 2, '{y}-{m}-{d} 00:00:00')), new Date(this.parseTime(new Date(), '{y}-{m}-{d} 23:59:59'))];
+    // this.receiveTime = [new Date(this.parseTime(new Date().getTime() - 24 * 60 * 60 * 1000 * 2, '{y}-{m}-{d} 00:00:00')), new Date(this.parseTime(new Date(), '{y}-{m}-{d} 23:59:59'))];
     this.queryParams.waybillNo = this.$route.query.waybillNo;
     await this.getList();
     this.currentId = this.wayBillCode;
